d0fe5eb00284f0f6e5a4cf6ffe816dd2b117a502
Molly Asher <m.asher@hotmail.com>
2020-04-28 16:13:50 +0100

    converting to be for osb

--- Animations/Animate_Obs.py
+++ Animations/Animate_Obs.py
@@ -33,35 +33,6 @@ end_year = 1992
 # Time constraint for which to test the data
 days_constraint = iris.Constraint(time=lambda cell: PartialDateTime(year = end_year, month=12, day=12) < cell.point < PartialDateTime(year = end_year, month=12, day=18))
 
-###############################################################################
-# Load in a timeseries for a specific location (data to be checked)
-###############################################################################
-# Load in the time series cube
-obs_ts_cube = iris.load(f'Outputs/TimeSeries_cubes/Obs_{start_year}-{end_year}.nc')[0]
-print(obs_ts_cube)
-# load in the time series dataframe
-#pr_ts_df = pd.read_csv(f"Outputs/TimeSeries/Pr_{start_year}-{end_year}_EM01.csv")
-#print(pr_ts_df)
-
-# Extract the data which matches the constraint
-obs_ts_cube = obs_ts_cube.extract(days_constraint)
-
-###############################################################################
-# Load in one month's worth of data in a cube for whole of country
-###############################################################################                               
-filename = "datadir/CEH-GEAR/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19821201-19821230.nc"
-month_uk_cube = iris.load(filename,'lwe_precipitation_rate')[0]
-# Remove ensemble member dimension
-month_uk_cube = month_uk_cube[0, :]
-
-# Extract the data which matches the constraint
-month_uk_cube = month_uk_cube.extract(days_constraint)
-
-###############################################################################
-# Plot the timeseries
-###############################################################################
-qplt.plot(pr_ts_cube)
-plt.show()
 
 ###############################################################################
 # Animate a month's worth of data over whole UK
@@ -83,16 +54,38 @@ min_value = month_uk_cube.data.min()  # Lowest value
 max_value = month_uk_cube.data.max()  # Highest value
 
 def draw(frame):
-    # Clear the previous figure
+    # Clear the previous figure, so that the colourbars dont overlay each other.
     plt.clf()
-    grid = month_uk_cube[frame]
-    levels = np.round(np.linspace(0.1, max_value, 50 ), 2 )
-    contour = qplt.contourf(grid,levels=levels,cmap=precip_colormap, extend="both")
-    plt.gca().coastlines(resolution='50m', color='black', linewidth=0.5)
-    plt.plot(0.6628091964140957, 1.2979678925914127, 'o', color='black', markersize = 3) 
+    # Extract one hour 
+    hour = one_day[frame]
+    #Extract the data
+    hour_data = hour.data
+    # Flip the data so it's not upside down
+    hour_data_fl = np.flipud(hour_data)
+    # Fill empty values with NaN
+    hour_data_fl = hour_data_fl.filled(np.nan)  
+
+    # Create the contour plot, with colourbar, with axes correctly spaced
+    contour = plt.contourf(hour_data_fl, cmap=precip_colormap)
+    contour = plt.colorbar()
+    contour =plt.axes().set_aspect('equal') 
+    #plt.gca().coastlines(resolution='50m', color='black', linewidth=0.5)
+    # Make mark at index of point closest to our point of interest
+    plt.plot(rv_closest_idx_fl[1], rv_closest_idx_fl[0], 'o', color='black', markersize = 3) 
+        
+    # Alternative using the Iris plotting functions
+    hour.data = np.flipud(hour.data)
+    contour = qplt.contourf(hour, cmap=precip_colormap, levels = levels)
+    contour = plt.axes().set_aspect('equal') 
+    plt.plot(rv_closest_idx_fl[1], rv_closest_idx_fl[0], 'o', color='black', markersize = 3) 
+    # Coastlines doesnt work as not plotting spatial coordinates
+    #plt.gca().coastlines(resolution='50m', color='black', linewidth=0.5)
+    #plt.plot(53.802070, -1.588941, 'o', color='black', markersize = 10) 
+      
     # Create datetime in human readable format
-    datetime = grid.coord('time').units.num2date(grid.coord('time').points[0]) 
-    title = u"%s — %s" % (month_uk_cube.long_name, str(datetime))
+    datetime = hour.coord('time').units.num2date(hour.coord('time').points[0]) 
+    
+    title = u"%s — %s" % (hour.long_name, str(datetime))
     plt.title(title)
     return contour
     
@@ -102,7 +95,9 @@ def init():
 def animate(frame):
     return draw(frame)
 
-ani = animation.FuncAnimation(fig, animate, frames, interval=500, save_count=50, blit=False, init_func=init,repeat=False)
-ani.save('Outputs/Animations/test.mp4', writer=animation.FFMpegWriter(fps=8))
+ani = animation.FuncAnimation(fig, animate, frames, interval=5000, blit=False, init_func=init,
+                              repeat=False)
+ani.save('C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/Outputs/test_obvs.mp4', writer=animation.FFMpegWriter(fps=8))
+#plt.close(fig)
 
 
