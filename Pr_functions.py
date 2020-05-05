d80f73eb4a1658f6d994d0523e1c5f1336579904
Molly Asher <m.asher@hotmail.com>
2020-05-05 09:17:33 +0100

    added function for finding multiple nearest neighbours

--- Pr_functions.py
+++ Pr_functions.py
@@ -120,19 +120,50 @@ def create_concat_cube_one_location_m3 (cube_list, sample_point):
      time_series = concat_cube.extract(iris.Constraint(grid_latitude=closest_lat, grid_longitude = closest_long))
      return (time_series)     
 
-def log_discrete_bins(min_value,max_value,bins_if_log_spaced,discretisation):
-    delta_log_i_l_s=(np.log10(max_value)-np.log10(min_value))/bins_if_log_spaced
-    bin_edges=[min_value]
-    prev_edge=min_value
-    lstopped=False
-    while(lstopped==False):
-        log10prev=np.log10(prev_edge)
-        if(log10prev>np.log10(max_value)):
-            lstopped=True
-        else:
-            next_delta=10**(log10prev+delta_log_i_l_s)-10**(log10prev)
-            # conservative estimate, round bin size to lower number
-            next_edge=prev_edge+max(discretisation,discretisation*int(next_delta/discretisation))
-            bin_edges.append(next_edge)
-            prev_edge=next_edge
-    return bin_edges
+
+def concat_cube_multiple_neighbours_m3 (cube_list, sample_point, n_nearest_neighbours):
+    # Remove attributes which aren't the same across all the cubes.
+     for cube in cube_list:
+         for attr in ['creation_date', 'tracking_id', 'history']:
+             if attr in cube.attributes:
+                 del cube.attributes[attr]
+     
+     # Concatenate the cubes into one
+     concat_cube = cube_list.concatenate_cube()
+     
+     # Reduce the dimensions (remove ensemble member dimension)
+     concat_cube = concat_cube[0, :]
+     
+     # Create a list of all the tuple pairs of latitude and longitudes
+     locations = list(itertools.product(concat_cube.coord('grid_latitude').points, concat_cube.coord('grid_longitude').points))
+     
+     # Correct them so that 360 merges back into one
+     corrected_locations = []
+     for location in locations:
+         if location[0] >360:
+             new_lat = location[0] -360
+         else: 
+             new_lat = location[0]
+         if location[1] >360:
+             new_long = location[1] -360     
+         else:
+             new_long = location[1]
+         new_location = new_lat, new_long 
+         corrected_locations.append(new_location)
+     
+     # Find the index of the nearest neighbour(s) of the sample point in the list of locations present in concat_cube
+     tree = spatial.KDTree(corrected_locations)
+     #closest_point_idx = tree.query([(sample_point[0][1], sample_point[1][1])])[1][0]
+     closest_points_idxs = tree.query([(sample_point[0][1], sample_point[1][1])], k =n_nearest_neighbours )[1][0]
+     
+     time_series_ls = []
+     for point in closest_points_idxs:
+         # Extract the lat and long values of this point using the index
+         closest_lat = locations[point][0]
+         closest_long = locations[point][1]
+         # Use this closest lat, long pair to collapse the latitude and longitude dimensions
+         # of the concatenated cube to keep just the time series for this closest point 
+         time_series = concat_cube.extract(iris.Constraint(grid_latitude=closest_lat, grid_longitude = closest_long))
+         time_series_ls.append(time_series)
+    
+     return (time_series_ls)  
