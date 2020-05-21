4e495662205e34817698941a20e21aeb6311ef62
Molly Asher <m.asher@hotmail.com>
2020-05-21 15:09:24 +0100

    added testing functionalities

--- shifting_grid.py
+++ shifting_grid.py
@@ -61,57 +61,52 @@ hour_uk_cube.coord_system()
 hour_uk_cube[0].coord('grid_latitude').coord_system.as_cartopy_crs()
 
 ##############################################################################
-# Get data
-data = hour_uk_cube.data
-data_1d = data.reshape(-1)
-
 # Extract lats and longs in rotated pol as a 2D array
-lats = hour_uk_cube.coord('grid_latitude').points
-lons = hour_uk_cube.coord('grid_longitude').points
-
-# Convert to 1d
-lons_1d = lons.reshape(-1)
-lats_1d = lats.reshape(-1)
+lats_rp_1d = hour_uk_cube.coord('grid_latitude').points
+lons_rp_1d = hour_uk_cube.coord('grid_longitude').points
 
 # Find the distance between each lat/lon and the next lat/lon
 # Divide this by two to get the distance to the half way point
-lat_differences = np.diff(lats_1d)
-lat_differences_half = lat_differences/2
-lon_differences = np.diff(lons_1d)
-lon_differences_half = lon_differences/2
+lats_rp_differences = np.diff(lats_rp_1d)
+lats_rp_differences_half = lats_rp_differences/2
+lons_rp_differences = np.diff(lons_rp_1d)
+lons_rp_differences_half = lons_rp_differences/2
 
 # Create an array of lats/lons at the midpoints
-lats_edit = lats_1d[0:len(lat_differences)] + lat_differences_half
-lons_edit = lons_1d[0:len(lon_differences)] + lon_differences_half
+lats_rp_midpoints_1d = lats_rp_1d[1:] - lats_rp_differences_half
+lons_rp_midpoints_1d = lons_rp_1d[1:] - lons_rp_differences_half
+
+# Convert to 2D
+lons_rp_midpoints_2d, lats_rp_midpoints_2d = np.meshgrid(lons_rp_midpoints_1d, lats_rp_midpoints_1d)
 
 # Convert to wgs84
-x, y = np.meshgrid(lons_edit, lats_edit)
 cs = hour_uk_cube.coord_system()
-lons_wgs84, lats_wgs84 = iris.analysis.cartography.unrotate_pole(x, y, cs.grid_north_pole_longitude, cs.grid_north_pole_latitude)
-
-# Create an array of the data values, trimmed to the size of the edited lats/lons
-#data = data[0:lons_wgs84.shape[0], 0:lons_wgs84.shape[1]]
-#plt.pcolor(lons_wgs84,lats_wgs84, data )
-#plt.scatter(lons_wgs84,lats_wgs84, color = 'blue')
-
+lons_wgs84_midpoints_2d, lats_wgs84_midpoints_2d = iris.analysis.cartography.unrotate_pole(lons_rp_midpoints_2d, lats_rp_midpoints_2d, cs.grid_north_pole_longitude, cs.grid_north_pole_latitude)
 # Convert to web mercator
-lons_wgs_84_1d = lons_wgs84.reshape(-1)
-lats_wgs_84_1d = lats_wgs84.reshape(-1)
-lons_wm_1d_v2, lats_wm_1d_v2 = transform(Proj(init='epsg:4326'),Proj(init='epsg:3785'),lons_wgs_84_1d,lats_wgs_84_1d)
+lons_wm_midpoints_2d, lats_wm_midpoints_2d = transform(Proj(init='epsg:4326'),Proj(init='epsg:3785'),lons_wgs84_midpoints_2d,lats_wgs84_midpoints_2d)
+# Convert to 1d for 
+lons_wm_midpoints_1d = lons_wm_midpoints_2d.reshape(-1)
+lats_wm_midpoints_1d = lats_wm_midpoints_2d.reshape(-1)
 
-# Reshape them to the shape of the grid
-lats_wm_2d_v2 = np.array(lats_wm_1d_v2).reshape(lons_wgs84.shape)
-lons_wm_2d_v2 = np.array(lons_wm_1d_v2).reshape(lons_wgs84.shape)  
+# Remove data points that were lost in this process
+#data = hour_uk_cube.data
+data = np.full((606, 484), 7, dtype=int)
+data = data[1:,1:]
+
+# Test array
+data = np.full((606, 484), 7, dtype=int)
+data[278,313] = 500
+lons_wm_midpoints_2d[278,311]
+lats_wm_midpoints_2d[278,313]
 
 ##############################################################################
 #### Find part of the rainfall cube which is within this geometry
 ##############################################################################
-lons_wm_1d_v1 = lons_wm_1d[0:len(lon_differences)]
-lats_wm_1d_v1 = lats_wm_1d[0:len(lat_differences)]
-
+#lons_wm_1d_v1 = lons_wm_1d[0:len(lon_differences)]
+#lats_wm_1d_v1 = lats_wm_1d[0:len(lat_differences)]
 # Check whether each lat, lon pair is within the geometry
 mask = []
-for lon, lat in zip(lons_wm_1d, lats_wm_1d):
+for lon, lat in zip(lons_wm_midpoints_1d, lats_wm_midpoints_1d):
     this_point = Point(lon, lat)
     res = this_point.within(leeds_poly)
     #res = leeds_poly.contains(this_point)
@@ -127,6 +122,8 @@ mask = mask.astype(int)
 mask = np.ma.masked_array(mask, mask < 1)
 
 
+plt.pcolormesh(lons_wm_midpoints_2d, lats_wm_midpoints_2d, data, linewidths=4)
+
 
 fig, ax = plt.subplots(figsize=(20,20))
 extent = tilemapbase.extent_from_frame(leeds, buffer=5)
@@ -134,9 +131,9 @@ plotter = tilemapbase.Plotter(extent, tilemapbase.tiles.build_OSM(), width=600)
 plotter.plot(ax)
 # Add points at corners of grids
 ax.plot(lons_wm_1d, lats_wm_1d, "bo", markersize =10)
-ax.plot(lons_wm_1d_v2, lats_wm_1d_v2, "ro", markersize =10)
+ax.plot(lons_wm_midpoints_1d, lats_wm_midpoints_1d, "ro", markersize =10)
 #ax.pcolormesh(lons_wm_2d, lats_wm_2d, mask, cmap =c.ListedColormap(['firebrick']), linewidths=4, alpha = 0.5, edgecolor = 'blue')
-ax.pcolormesh(lons_wm_2d_v2, lats_wm_2d_v2, mask, cmap =c.ListedColormap(['firebrick']), linewidths=4, alpha = 0.5, edgecolor = 'blue')
+ax.pcolormesh(lons_wm_midpoints_2d, lats_wm_midpoints_2d, data)
 leeds.plot(ax=ax, categorical=True, alpha=0.9, edgecolor='firebrick', color='none', linewidth=6)
 ax.tick_params(labelsize='xx-large')
 
@@ -155,3 +152,5 @@ ax.tick_params(labelsize='xx-large')
 
 
 
+test = data
+test[]
