fda9a5dd97059d4df24dc9e9a7393df04ced8952
Molly Asher <m.asher@hotmail.com>
2020-07-27 17:05:19 +0100

    trying to set up to run for multiple ems

--- SpatialAnalyses/RegionalAMAXforR.py
+++ SpatialAnalyses/RegionalAMAXforR.py
@@ -33,7 +33,9 @@ from Spatial_plotting_functions import *
 start_year = 1980
 end_year = 2000 
 yrs_range = "1980_2001" 
-em = '01'
+ems = ['01', '03']
+region = 'Northern'
+
 
 ############################################
 # Create regions
@@ -55,125 +57,137 @@ regional_gdf = northern_gdf
 #############################################
 # Read in files
 #############################################
-# Create list of names of cubes for between the years specified
-filenames =[]
-for year in range(start_year,end_year+1):
-    # Create filepath to correct folder using ensemble member and year
-    general_filename = 'datadir/UKCP18/2.2km/{}/{}/pr_rcp85_land-cpm_uk_2.2km_{}_1hr_{}*'.format(em, yrs_range, em, year)
-    #print(general_filename)
-    # Find all files in directory which start with this string
-    for filename in glob.glob(general_filename):
-        #print(filename)
-        filenames.append(filename)
 
-#filenames =[]
-#filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19801201-19801230.nc')  
-#filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19810101-19810130.nc') 
-#filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19820601-19820630.nc') 
-#filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19830601-19830630.nc') 
+for em in ems:
 
-monthly_cubes_list = iris.load(filenames,'lwe_precipitation_rate')
-print(str(len(monthly_cubes_list)) + " cubes found for this time period.")
-
-#############################################
-# Concat the cubes into one
-#############################################
-# Remove attributes which aren't the same across all the cubes.
-for cube in monthly_cubes_list:
-     for attr in ['creation_date', 'tracking_id', 'history']:
-         if attr in cube.attributes:
-             del cube.attributes[attr]
- 
- # Concatenate the cubes into one
-concat_cube = monthly_cubes_list.concatenate_cube()
-#
-# Remove ensemble member dimension
-concat_cube = concat_cube[0,:,:,:]
-
-#############################################
-# Trim the cube to the BBOX of the region of interest
-#############################################
-regional_cube = trim_to_bbox_of_region(concat_cube, northern_gdf)
-
-# Check plotting
-qplt.contourf(regional_cube[10,:,:])       
-plt.gca().coastlines()   
-# Check plotting #.2
-#plot_cube_within_region(regional_cube[112,:,:], northern_gdf)
-
-#############################################
-# 
-#############################################
-# Create a masked array - masking out all cells not within the region 
-regional_mask = trim_to_gdf(regional_cube, northern_gdf)
-# Copy the original cube (so as changes arent implemented in original cube as well)
-masked_regional_cube = regional_cube.copy()
-# Set cubes data with the mask
-masked_regional_cube.data = regional_mask
-
-# Check plotting
-qplt.contourf(masked_regional_cube[10,:,:])       
-plt.gca().coastlines()   
-
-############################################
-# Find the maximum value in each June-July_August period
-#############################################
-## Add season and season_year variables
-iris.coord_categorisation.add_season_year(masked_regional_cube,'time', name = "season_year")
-iris.coord_categorisation.add_season(masked_regional_cube,'time', name = "clim_season")
-
-# Aggregate to get just the maximum value in each seasonal yearly period
-annual_seasonal_max = masked_regional_cube.aggregated_by(['season_year', 'clim_season'], iris.analysis.MAX)
-print(annual_seasonal_max)
-
-# Keep only JJA
-jja = annual_seasonal_max.extract(iris.Constraint(clim_season = 'jja'))
-
- 
-############################################
-# Check plotting
-#############################################
-qplt.contourf(jja[1,:,:])       
-plt.gca().coastlines()  
-plot_cube_within_region(jja, regional_gdf)
-
-############################################
-# Reformat for use in R
-#############################################
-# Get the coords 1D
-lats = jja.coord('grid_latitude').points
-lons = jja.coord('grid_longitude').points
-
-# Convert to 2D
-lats_2d, lons_2d = np.meshgrid(lats, lons)
-
-# Convert to 1D
-lats_1d = lats_2d.reshape(-1)
-lons_1d = lons_2d.reshape(-1)
-
-# Create dataframe
-df = pd.DataFrame({'lons': lons_1d, "lats": lats_1d})
-
-#############################
-my_dict = {}
-for i in range(0, jja.shape[0]):
-    print(i)
-    # Get data from one timeslice
-    one_ts = jja[i,:,:]
-    # Extract data from one year 
-    data = one_ts.data.reshape(-1)
-    year  = one_ts.coord('season_year').points[0]
-    # Store as dictionary with the year name
-    my_dict[year] = data
-
-# Create as a dataframe
-test = pd.DataFrame(my_dict)
-
-# Remove NA rows
-test = test.dropna()
-
-
-
+    # Create list of names of cubes for between the years specified
+    filenames =[]
+    for year in range(start_year,end_year+1):
+        # Create filepath to correct folder using ensemble member and year
+        general_filename = 'datadir/UKCP18/2.2km/{}/{}/pr_rcp85_land-cpm_uk_2.2km_{}_1hr_{}*'.format(em, yrs_range, em, year)
+        #print(general_filename)
+        # Find all files in directory which start with this string
+        for filename in glob.glob(general_filename):
+            #print(filename)
+            filenames.append(filename)
     
-
-
+    #filenames =[]
+    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19801201-19801230.nc')  
+    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19810101-19810130.nc') 
+    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19820601-19820630.nc') 
+    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19830601-19830630.nc') 
+    
+    monthly_cubes_list = iris.load(filenames,'lwe_precipitation_rate')
+    print(str(len(monthly_cubes_list)) + " cubes found for this time period.")
+    
+    #############################################
+    # Concat the cubes into one
+    #############################################
+    # Remove attributes which aren't the same across all the cubes.
+    for cube in monthly_cubes_list:
+         for attr in ['creation_date', 'tracking_id', 'history']:
+             if attr in cube.attributes:
+                 del cube.attributes[attr]
+     
+     # Concatenate the cubes into one
+    concat_cube = monthly_cubes_list.concatenate_cube()
+    #
+    # Remove ensemble member dimension
+    concat_cube = concat_cube[0,:,:,:]
+    
+    #############################################
+    # Trim the cube to the BBOX of the region of interest
+    #############################################
+    regional_cube = trim_to_bbox_of_region(concat_cube, northern_gdf)
+    
+    # Check plotting
+    qplt.contourf(regional_cube[10,:,:])       
+    plt.gca().coastlines()   
+    # Check plotting #.2
+    #plot_cube_within_region(regional_cube[112,:,:], northern_gdf)
+    
+    #############################################
+    # 
+    #############################################
+    
+    if not 'regional_mask' in globals():
+        # Create a masked array - masking out all cells not within the region 
+        regional_mask = trim_to_gdf(regional_cube, northern_gdf)
+    # Copy the original cube (so as changes arent implemented in original cube as well)
+    masked_regional_cube = regional_cube.copy()
+    # Set cubes data with the mask
+    masked_regional_cube.data = regional_mask
+    
+    # Check plotting
+    qplt.contourf(masked_regional_cube[10,:,:])       
+    plt.gca().coastlines()   
+    
+    ############################################
+    # Find the maximum value in each June-July_August period
+    #############################################
+    ## Add season and season_year variables
+    iris.coord_categorisation.add_season_year(masked_regional_cube,'time', name = "season_year")
+    iris.coord_categorisation.add_season(masked_regional_cube,'time', name = "clim_season")
+    
+    # Aggregate to get just the maximum value in each seasonal yearly period
+    annual_seasonal_max = masked_regional_cube.aggregated_by(['season_year', 'clim_season'], iris.analysis.MAX)
+    print(annual_seasonal_max)
+    
+    # Keep only JJA
+    jja = annual_seasonal_max.extract(iris.Constraint(clim_season = 'jja'))
+    
+     
+    ############################################
+    # Check plotting
+    #############################################
+    qplt.contourf(jja[1,:,:])       
+    plt.gca().coastlines()  
+    plot_cube_within_region(jja, regional_gdf)
+    
+    ############################################
+    # Reformat for use in R
+    #############################################
+    # Get the coords 1D
+    lats = jja.coord('grid_latitude').points
+    lons = jja.coord('grid_longitude').points
+    
+    # Convert to 2D
+    lats_2d, lons_2d = np.meshgrid(lats, lons)
+    
+    # Convert to 1D
+    lats_1d = lats_2d.reshape(-1)
+    lons_1d = lons_2d.reshape(-1)
+    
+    # Create dataframe
+    df = pd.DataFrame({'lons': lons_1d, "lats": lats_1d})
+    
+    #############################
+    # Create a dictionary with each key corresponding to a year and the values
+    # containing a 1D array of AMAX values, masked to the region of interest
+    my_dict = {}
+    for i in range(0, jja.shape[0]):
+        print(i)
+        # Get data from one timeslice
+        one_ts = jja[i,:,:]
+        # Extract data from one year 
+        data = one_ts.data.reshape(-1)
+        year  = one_ts.coord('season_year').points[0]
+        # Store as dictionary with the year name
+        my_dict[year] = data
+    
+    # Create as a dataframe
+    test = pd.DataFrame(my_dict)
+    
+    # Remove NA rows
+    test = test.dropna()
+    
+    
+    # Save dataframe
+    ddir = root_fp + "Outputs/DataforR/{}/".format(region)
+    if not os.path.isdir(filepath_to_save):
+        os.makedirs(filepath_to_save)
+    test.to_csv(ddir + "em{}.csv".format(em), index = False)
+    
+    
+    
+    
