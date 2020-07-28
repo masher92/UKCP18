f29b98afc0f4634c229e8f5c96cbf5245abb74da
Molly Asher <gy17m2a@leeds.ac.uk>
2020-07-28 16:56:22 +0100

    .

--- SpatialAnalyses/RegionalAMAXforR.py
+++ SpatialAnalyses/RegionalAMAXforR.py
@@ -35,8 +35,9 @@ from Spatial_plotting_functions import *
 start_year = 1980
 end_year = 2000 
 yrs_range = "1980_2001" 
-ems = ['04','05', '06', '07', '08', '09', '10', '11']# '12', '13', '15']
-region = 'WY'
+#ems = ['04','05', '06', '07', '08', '09', '10', '11']# '12', '13', '15']
+ems = ['06']
+region = 'Northern'
 
 ############################################
 # Create regions
@@ -53,7 +54,7 @@ northern_gdf = northern_gdf.to_crs({'init' :'epsg:3785'})
 northern_gdf['merging_col'] = 0
 northern_gdf = northern_gdf.dissolve(by='merging_col')
 
-regional_gdf = wy_gdf
+regional_gdf = northern_gdf
 
 #############################################
 # Read in files
@@ -114,15 +115,15 @@ for em in ems:
     #############################################
     # 
     #############################################
-    seconds = time.time()
-    if not 'regional_mask' in globals():
-        # Create a masked array - masking out all cells not within the region 
-        regional_mask = mask_by_region(regional_cube, regional_gdf)
-        print('Created regional_mask in: ', time.time() - seconds)
-    else: 
-        print('Regional mask already exists')
+    #seconds = time.time()
+    #if not 'regional_mask' in globals():
+    #    # Create a masked array - masking out all cells not within the region 
+    #    regional_mask = mask_by_region(regional_cube, regional_gdf)
+    #    print('Created regional_mask in: ', time.time() - seconds)
+    #else: 
+    #    print('Regional mask already exists')
     # Copy the original cube (so as changes arent implemented in original cube as well)
-    masked_regional_cube = regional_cube.copy()
+    #masked_regional_cube = regional_cube.copy()
     # Set cubes data with the mask
     #print("Masking cubes")
     #seconds = time.time()
@@ -141,63 +142,46 @@ for em in ems:
     iris.coord_categorisation.add_season(masked_regional_cube,'time', name = "clim_season")
     
     # Keep only JJA
-    jja = masked_regional_cube.extract(iris.Constraint(clim_season = 'jja'))
+    jja = regional_cube.extract(iris.Constraint(clim_season = 'jja'))
     
     # Aggregate to get just the maximum value in each seasonal yearly period
     seconds = time.time()
-    annual_seasonal_max = jja.aggregated_by(['season_year'], iris.analysis.MAX)
+    jja_max = jja.aggregated_by(['season_year', 'clim_season'], iris.analysis.MAX)
     print('Found annual seasonal max in: ', time.time() - seconds)
     
     # Mask JJA
-    seconds = time.time()
-    mask_3d = np.repeat(regional_mask[np.newaxis,:, :], annual_seasonal_max.shape[0], axis=0)
+    #seconds = time.time()
+    #mask_3d = np.repeat(regional_mask[np.newaxis,:, :], annual_seasonal_max.shape[0], axis=0)
     #print("Seconds to run =", time.time() - seconds)	
-    annual_seasonal_max.data =  np.ma.masked_array(annual_seasonal_max.data, np.logical_not(mask_3d))
+    #annual_seasonal_max.data =  np.ma.masked_array(annual_seasonal_max.data, np.logical_not(mask_3d))
     
     ############################################
     # Check plotting
     #############################################
     #qplt.contourf(jja[1,:,:])       
     #plt.gca().coastlines()  
-    plot_cube_within_region(jja, regional_gdf)
+    #plot_cube_within_region(jja, regional_gdf)
     
     ############################################
     # Reformat for use in R
     #############################################
     # Get the coords 1D
-    # lats = jja.coord('grid_latitude').points
-    # lons = jja.coord('grid_longitude').points
-    
-    # # Convert to 2D
-    # lats_2d, lons_2d = np.meshgrid(lats, lons)
-    
-    # # Convert to 1D
-    # lats_1d = lats_2d.reshape(-1)
-    # lons_1d = lons_2d.reshape(-1)
-    
-    # Get the coords 1D
-    lats_1d = jja.coord('latitude').points
-    lons_1d = jja.coord('longitude').points
-    
-    # Convert to 2D
-    #lats_2d, lons_2d = np.meshgrid(lats, lons)
+    lats_1d = jja_max.coord('latitude').points
+    lons_1d = jja_max.coord('longitude').points
     
     # Convert to 1D
     lats_1d = lats_1d.reshape(-1)
     lons_1d = lons_1d.reshape(-1)
     
-    
-    # Create dataframe
-    #df = pd.DataFrame({'lons': lons_1d, "lats": lats_1d})
-    
     #############################
+    print("Creating dictionary")
     # Create a dictionary with each key corresponding to a year and the values
     # containing a 1D array of AMAX values, masked to the region of interest
     my_dict = {}
-    for i in range(0, jja.shape[0]):
-        #print(i)
+    for i in range(0, jja_max.shape[0]):
+        print(i)
         # Get data from one timeslice
-        one_ts = jja[i,:,:]
+        one_ts = jja_max[i,:,:]
         # Extract data from one year 
         data = one_ts.data.reshape(-1)
         year  = one_ts.coord('season_year').points[0]
@@ -214,11 +198,19 @@ for em in ems:
     test = test.dropna()
     
     # Save dataframe
+    print("Saving output")
     ddir = "Outputs/DataforR/{}/".format(region)
     if not os.path.isdir(ddir):
         os.makedirs(ddir)
     test.to_csv(ddir + "em{}.csv".format(em), index = False)
     
     print("Finished everything for EM in: ", time.time() - start_time)	
+    
+    
+    
+    
+qplt.contourf(jja_max[1,:,:])       
+plt.gca().coastlines()  
+plot_cube_within_region(one_ts, regional_gdf)    
     
     
