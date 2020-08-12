67ea6bcf5285d3e6a742a41ad85b7ef94e4bb0a1
Molly Asher <gy17m2a@leeds.ac.uk>
2020-08-12 12:03:10 +0100

    edited the percentile so its correct, now code gives top m20 valeus

--- SpatialAnalyses/RegionalAMAXforR.py
+++ SpatialAnalyses/RegionalAMAXforR.py
@@ -36,7 +36,7 @@ from Spatial_plotting_functions import *
 start_year = 1980
 end_year = 2000 
 yrs_range = "1980_2001" 
-ems = ['01', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '15']
+ems = ['05', '06', '07', '08', '09', '10', '11', '12', '13', '15']
 #ems = ['15']
 region = 'Northern'
 
