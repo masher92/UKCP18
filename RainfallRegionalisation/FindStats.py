041b977c4787a25aa07af8a1e9ed29d7408bac9d
Molly Asher <m.asher@hotmail.com>
2020-08-20 15:31:26 +0100

    adding documentation

--- RainfallRegionalisation/FindStats.py
+++ RainfallRegionalisation/FindStats.py
@@ -1,3 +1,8 @@
+'''
+Finds max, mean, percentiles and greatest ten values for every cell in the cube
+which is within the bounding box of the North of England.
+'''
+
 import iris.coord_categorisation
 import iris
 import glob
@@ -10,7 +15,7 @@ import time
 import sys
 
 """Using numba to extract values above a percentile.
-Numba executes loops efficiently
+Numba executes loops efficiently (just in time compiling)
 Somehow, the exceptions do not work well within numba, so they are passed out as integers"""
 @jit
 def values_above_percentile(rain_data,percentile_data,n_highest):
@@ -42,17 +47,27 @@ def values_above_percentile(rain_data,percentile_data,n_highest):
     return n_highest_array,exception
 
 
+############################################
+# Define variables and set up environment
+#############################################
 root_fp = "/nfs/a319/gy17m2a/"
 os.chdir(root_fp)
 
-ems = ['04', '05', '06', '07', '08', '09','10','11','12', '13','15']
-#ems = ['11']
+# Create path to files containing functions
+sys.path.insert(0, root_fp + 'Scripts/UKCP18/')
+from Pr_functions import *
+sys.path.insert(0, root_fp + 'Scripts/UKCP18/SpatialAnalyses')
+from Spatial_plotting_functions import *
+
+ems = ['01','04', '05', '06', '07', '08', '09','10','11','12', '13','15']
 years = range(1981,2000)  
 n_highest=20
-temp_perc_file='/nfs/a319/gy17m2a/Outputs/temp_stats_percentile.nc'
+#temp_perc_file='/nfs/a319/gy17m2a/Outputs/temp_stats_percentile.nc'
 
-region = 'Northern'
-# Create geodataframe of West Yorks
+############################################
+# Create a GDF for Northern England
+#############################################
+# Create geodataframe of Northrn England
 uk_gdf = gpd.read_file("datadir/SpatialData/Region__December_2015__Boundaries-shp/Region__December_2015__Boundaries.shp") 
 regional_gdf = uk_gdf.loc[uk_gdf['rgn15nm'].isin(['North West', 'North East', 'Yorkshire and The Humber'])]
 regional_gdf = regional_gdf.to_crs({'init' :'epsg:3785'}) 
@@ -60,14 +75,12 @@ regional_gdf = regional_gdf.to_crs({'init' :'epsg:3785'})
 regional_gdf['merging_col'] = 0
 regional_gdf = regional_gdf.dissolve(by='merging_col')
 
-# Create path to files containing functions
-sys.path.insert(0, root_fp + 'Scripts/UKCP18/')
-from Pr_functions import *
-sys.path.insert(0, root_fp + 'Scripts/UKCP18/SpatialAnalyses')
-from Spatial_plotting_functions import *
- 
+############################################
+# 
+#############################################
 for em in ems:
     print(em)
+    # Create dictionaries to store results
     top_ten_dict = {}
     mean_dict = {}
     max_dict = {}
@@ -75,9 +88,13 @@ for em in ems:
     percentile2_dict ={}
     percentile3_dict ={}
     percentile4_dict ={}
+    # Loop through years
     for this_year in years:
         year=str(this_year)
         print(year)
+        #############################################
+        ## Load in the data
+        #############################################
         filenames=glob.glob('datadir/UKCP18/2.2km/'+em+'/1980_2001/pr_rcp85_land-cpm_uk_2.2km_' +em+'_1hr_'+year+'*.nc')
         monthly_cubes_list = iris.load(filenames,'lwe_precipitation_rate')
         for cube in monthly_cubes_list:
@@ -92,29 +109,22 @@ for em in ems:
         concat_cube = concat_cube[0,:,:,:]
      
         #############################################
-        # Trim the cube to the BBOX of the region of interest
+        # Trim the cube to the BBOX of the North of England 
         #############################################
         seconds = time.time()
         regional_cube = trim_to_bbox_of_region(concat_cube, regional_gdf)
         print("Trimmed to extent of bbox in: ", time.time() - seconds)
-        
-        #### Mask data     
-        # if mask_to_region == True:   
-        #   seconds = time.time()
-        #   regional_cube = mask_by_region(regional_cube, regional_gdf)
-        #   print('Created regional_mask in: ', time.time() - seconds)
-        #   mask_3d = np.repeat(regional_mask[np.newaxis,:, :], regional_cube.shape[0], axis=0)
-        #   print("Seconds to run =", time.time() - seconds)	
-        #   regional_cube.data =  np.ma.masked_array(regional_cube.data, np.logical_not(mask_3d))
-        #   print ("Joined cubes into one")
-        
-        # Note we are dealing with one year at a time already
+    
+        #############################################
+        # Add season coordinates and trim to JJA
+        #############################################              
         iris.coord_categorisation.add_season(regional_cube,'time', name = "clim_season")
         jja = regional_cube.extract(iris.Constraint(clim_season = 'jja'))
         iris.coord_categorisation.add_season_year(jja,'time', name = "season") 
-        rain_data=jja.data
-                       
-        ### Find Max, mean
+                   
+        #############################################
+        # Find Max, mean, percentiles
+        #############################################   
         jja_mean = jja.aggregated_by(['season'], iris.analysis.MEAN)
         jja_max = jja.aggregated_by(['season'], iris.analysis.MAX)
         jja_percentiles = jja.aggregated_by(['season'], iris.analysis.PERCENTILE, percent=[95,97,99,99.5])
@@ -130,6 +140,10 @@ for em in ems:
         percentile3_dict[year] = percentile_3.data.reshape(-1)
         percentile4_dict[year] = percentile_4.data.reshape(-1)
         
+        #############################################
+        # Find greatest N values
+        #############################################   
+        rain_data=jja.data
         # Make a conservative estimate (it is a bit annoying to deal with the rounding issues)
         cutoff_percentile=100.*(1.0-(n_highest+1.0)/(np.shape(rain_data)[0]-1.0))
         yearly_stats_percentile = jja.aggregated_by(['season'], iris.analysis.PERCENTILE, percent=cutoff_percentile)
@@ -143,10 +157,9 @@ for em in ems:
         if(exception==2):
             raise Exception('Cutoff percentile generates too few data points')
          
-        ######## Store in format for R  
+        ######## Store in format for R, and add to dictionary
         # Remove Ensemble member dimension 
         data = n_highest_array [0,:,:,:]           
-        
         
         # Loops through each of the top ten values and store in a dictionary
         # with the year name
@@ -172,8 +185,6 @@ for em in ems:
     # Add lats and lons
     lats= jja.coord('latitude').points.reshape(-1)
     lons =  jja.coord('longitude').points.reshape(-1)   
-    
-    #
     top_ten_df['lat'], top_ten_df['lon'] = lats, lons
     mean_df['lat'], mean_df['lon'] = lats, lons
     max_df['lat'], max_df['lon'] = lats, lons
@@ -191,31 +202,5 @@ for em in ems:
     percentile3_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/99th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
     percentile4_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/99.5th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
     
-    
-    #     # Save the percentile data, open it with xarray later on
-    #     # Remove the temporary file with percentile data if it exists (need to figure out a better solution to ensure this file is closed)
-    #     if os.path.exists(temp_perc_file):
-    #         os.remove(temp_perc_file)
-    #     iris.save(yearly_stats_percentile,temp_perc_file)
-    #     # Save the results using xarray (easier for me to add a new dimension)
-    #     percentile_ds=xr.open_dataset(temp_perc_file)
-    #     # Add the "rank_in_season" dimension
-    #     n_highest_ds=xr.Dataset(
-    #     coords={
-    #         "time": percentile_ds.time,
-    #         "rank_in_season": ("rank_in_season", np.arange(1,np.shape(n_highest_array)[1]+1), {"long_name": "rank_in_season", "units": "-"},),
-    #         "grid_latitude": percentile_ds.grid_latitude,
-    #         "grid_longitude": percentile_ds.grid_longitude,
-    #     })
-    #     n_highest_ds["time"]=percentile_ds["time"]
-    #     n_highest_ds["latitude"]=("grid_latitude", "grid_longitude"),percentile_ds["latitude"]
-    #     n_highest_ds["longitude"]=("grid_latitude", "grid_longitude"),percentile_ds["longitude"]
-    #     n_highest_ds["pr_n_highest"]=("time","rank_in_season","grid_latitude","grid_longitude"),n_highest_array
-    #     # Merge dataset with that of previous years
-    #     n_highest_years_ds=xr.merge((n_highest_years_ds, n_highest_ds), compat = 'override')
-    # # The encoding here is used to ensure ncview is happy with the data
-    #     output_fp ='/nfs/a319/gy17m2a/Outputs/mem_'+ em+ '_pr_n_highest.nc'
-    
-    # n_highest_years_ds.to_netcdf(output_fp, encoding={'rank_in_season': {'dtype': 'i4'}})
-    
+
 
