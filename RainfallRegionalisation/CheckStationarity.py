fc349ba53668cca134b55e792e08101ff11b0c93
Molly Asher <m.asher@hotmail.com>
2020-08-26 09:16:05 +0100

    output plots and a table of stats

--- RainfallRegionalisation/CheckStationarity.py
+++ RainfallRegionalisation/CheckStationarity.py
@@ -1,29 +1,23 @@
 import sys
 import iris
-import cartopy.crs as ccrs
 import os
-from scipy import spatial
-import itertools
-import iris.quickplot as qplt
-import warnings
-import copy
-from timeit import default_timer as timer
 import glob
 import numpy as np
-import iris.quickplot as qplt
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
-import tilemapbase
-import numpy as np
-from shapely.geometry import Polygon
-import iris.coord_categorisation
 import time 
-import bottleneck
+from statsmodels.tsa.stattools import adfuller
 
 # Provide root_fp as argument
 #root_fp = "C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/"
 root_fp = "/nfs/a319/gy17m2a/"
+os.chdir(root_fp)
+
+# Create path to files containing functions
+sys.path.insert(0, root_fp + 'Scripts/UKCP18/')
+from Pr_functions import *
+sys.path.insert(0, root_fp + 'Scripts/UKCP18/SpatialAnalyses')
+from Spatial_plotting_functions import *
 
 # Create geodataframe of West Yorks
 uk_gdf = gpd.read_file("datadir/SpatialData/Region__December_2015__Boundaries-shp/Region__December_2015__Boundaries.shp") 
@@ -36,6 +30,8 @@ northern_gdf = northern_gdf.dissolve(by='merging_col')
 #############################################
 # Read in files
 #############################################
+adf_stats = []
+
 for em in ems:
     start_time = time.time()
     print ("Ensemble member {}".format(em))
@@ -51,11 +47,11 @@ for em in ems:
             #print(filename)
             filenames.append(filename)
     
-    #filenames =[]
-    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19801201-19801230.nc')  
-    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19810101-19810130.nc') 
-    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19820601-19820630.nc') 
-    #filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19830601-19830630.nc') 
+    # filenames =[]
+    # filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19801201-19801230.nc')  
+    # filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19810101-19810130.nc') 
+    # filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19820601-19820630.nc') 
+    # filenames.append(root_fp + 'datadir/UKCP18/2.2km/01/1980_2001/pr_rcp85_land-cpm_uk_2.2km_01_1hr_19830601-19830630.nc') 
     
     monthly_cubes_list = iris.load(filenames,'lwe_precipitation_rate')
     print(str(len(monthly_cubes_list)) + " cubes found for this time period.")
@@ -80,7 +76,7 @@ for em in ems:
     # Trim the cube to the BBOX of the region of interest
     #############################################
     seconds = time.time()
-    regional_cube = trim_to_bbox_of_region(concat_cube, regional_gdf)
+    regional_cube = trim_to_bbox_of_region(concat_cube, northern_gdf)
     print("Trimmed to extent of bbox in: ", time.time() - seconds)
 
     #############################################
@@ -88,6 +84,20 @@ for em in ems:
     #############################################              
     iris.coord_categorisation.add_season(regional_cube,'time', name = "clim_season")
     jja = regional_cube.extract(iris.Constraint(clim_season = 'jja'))
+    
+    
+    #############################################
+    # Plot
+    ############################################# 
+    df = pd.DataFrame(regional_mean_data)
+    df['year'] = jja.coord('year').points
+    
+    fig=plt.figure() 
+    plt.style.use('ggplot')
+    ax = df[0].plot()
+    #ax.set_xlabel("Areas",fontsize=12)
+    ax.set_xticklabels(df['year'], rotation=45)
+    fig.savefig("Outputs/Stationarity/em{}.png".format(em),bbox_inches='tight')
     
     #############################################
     # Check stationarity
@@ -111,6 +121,12 @@ for em in ems:
         print ("Time series not stationary")
     else:
         print("Time series is stationary")
-        
-
     
+    
+    # Add stats to a list of dataframes
+    df = pd.DataFrame([{'EM': em,'ADF Statistic': result[0],'PValue' : result[1]}])
+    adf_stats.append(df)
+    
+# Join list of dataframes 
+test = pd.concat(adf_stats, axis=0)
+test.to_csv("Outputs/Stationarity/ADF_stats.csv", index=False)
