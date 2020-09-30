768a888eb458755b2cbb7f06ee6cc9399abac3c7
Molly Asher <gy17m2a@leeds.ac.uk>
2020-09-30 15:33:05 +0100

    changes to this file due to discrpeancy noticed between max value in various ensemble members between the HiClimR_inputdata and the UK_stats plotting data. Something to do with the method for trimming to the region. This is now edited to be the same as the UK_Stats method

--- RainfallRegionalisation/CreateRegionalMasks.py
+++ RainfallRegionalisation/CreateRegionalMasks.py
@@ -19,6 +19,12 @@ import iris.coord_categorisation
 import time 
 warnings.filterwarnings("ignore")
 
+def variablename(var):
+     import itertools
+     return [tpl[0] for tpl in 
+     filter(lambda x: var is x[1], globals().items())]
+ 
+ 
 # Set working directory - 2 options for remote server and desktop
 #root_fp = "C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/"
 root_fp = "/nfs/a319/gy17m2a/"
@@ -34,10 +40,11 @@ from Spatial_plotting_functions import *
 #############################################
 wys_gdf = gpd.read_file("datadir/SpatialData/combined-authorities-april-2015-super-generalised-clipped-boundaries-in-england.shp") 
 wys_gdf = wys_gdf[wys_gdf['cauth15cd'] == 'E47000003']
-wys_gdf = wys_gdf.to_crs({'init' :'epsg:3785'}) 
 # Create the square around it
 bounding_box = wys_gdf.envelope
 wys_gdf = gpd.GeoDataFrame(gpd.GeoSeries(bounding_box), columns=['geometry'])
+wys_gdf.crs = "epsg:4326"
+wys_gdf = wys_gdf.to_crs({'init' :'epsg:3785'}) 
 
 # Create geodataframe of West Yorks
 uk_gdf = gpd.read_file("datadir/SpatialData/Region__December_2015__Boundaries-shp/Region__December_2015__Boundaries.shp") 
@@ -64,33 +71,41 @@ cube = cube[0,:,:,:]
 print(cube)
 
 #############################################
-# Trim the cube to the BBOX of the region of interest
+# Create a dataframe with the latitude and longitude of all locations in the
+# bounding box of the northern England region
 #############################################
-regional_cube = trim_to_bbox_of_region(cube, northern_gdf)
+square_northern_cube = trim_to_bbox_of_region(cube, northern_gdf)
+square_northern_cube_df = pd.DataFrame({'lat': square_northern_cube.coord('latitude').points.reshape(-1),
+                             'lon': square_northern_cube.coord('longitude').points.reshape(-1)})
 
-#############################################
-# Trim to the smaller areas
-#############################################
-def create_mask_df (regional_gdf, regional_cube):
-    masked_cube = mask_by_region(regional_cube, regional_gdf)
-    #masked_cube.shape
-    cube_mask_df = pd.DataFrame({'mask': masked_cube.reshape(-1), 
-                                       'lat': regional_cube.coord('latitude').points.reshape(-1),
-                                       'lon': regional_cube.coord('longitude').points.reshape(-1)})
-    cube_mask_df.replace(0, np.nan, inplace=True)
-    return(cube_mask_df)
 
-# Create
-northern_mask_df = create_mask_df(northern_gdf, regional_cube)
-leeds_at_centre_mask_df = create_mask_df(leeds_at_centre_gdf, regional_cube)
-wys_mask_df = create_mask_df(wys_gdf, regional_cube)
+for location in ["WY_square", "Northern", "leeds-at-centre"]:
+ 
+    if location == 'WY_square':
+        location_gdf = wys_gdf
+    elif location =='Northern':
+        location_gdf = northern_gdf
+    elif location == 'leeds-at-centre':
+        location_gdf = leeds_at_centre_gdf
+        
+    #############################################
+    # Create a dataframe with the latitude and longitude of all locations in the
+    # area of interest, along with a column marking them all to be within the region
+    # (this is required for joining purposes)
+    #############################################
+    # Create a cube trimmed to the region of interest
+    regional_cube = trim_to_bbox_of_region(square_northern_cube, location_gdf)
+    # Convert this to dataframe format
+    cube_mask_df = pd.DataFrame({'within_region': 1,
+                                   'lat': regional_cube.coord('latitude').points.reshape(-1),
+                                   'lon': regional_cube.coord('longitude').points.reshape(-1)})
+    
+    #############################################
+    # Join this to the dataframe for the whole of Northern England BBOX
+    # Latitude and Longitudes outwith the area of interest will have NA for column
+    # specifying whether they are within the region
+    #############################################
+    joined = square_northern_cube_df.merge(cube_mask_df,  on=['lat', 'lon'], how="left")
 
-# Check number of values
-leeds_at_centre_mask_df['mask'].value_counts()
-northern_mask_df['mask'].value_counts()
-wys_mask_df['mask'].value_counts()
-
-# Save to file
-northern_mask_df.to_csv("Outputs/RegionalMasks/Northern_mask.csv" , index = False, float_format='%.20f')
-leeds_at_centre_mask_df.to_csv("Outputs/RegionalMasks/leeds-at-centre_mask.csv" , index = False, float_format='%.20f')
-wys_mask_df.to_csv("Outputs/RegionalMasks/WY_square_mask.csv" , index = False, float_format='%.20f')
+    # Save to file
+    joined.to_csv("Outputs/RegionalMasks/{}_mask.csv".format(location) , index = False, float_format='%.20f')
