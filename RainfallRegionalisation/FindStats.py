14596c3d009660d3e5c36e6489ff106a44fbd748
Molly Asher <gy17m2a@leeds.ac.uk>
2020-09-30 15:36:57 +0100

    changes to this file due to discrpeancy noticed between max value in various ensemble members between the HiClimR_inputdata and the UK_stats plotting data. Realised it was because the years specified in the file only covered up to 1999 when there is actually data for 2000

--- RainfallRegionalisation/FindStats.py
+++ RainfallRegionalisation/FindStats.py
@@ -54,8 +54,8 @@ def values_above_percentile(rain_data,percentile_data,n_highest):
 ############################################
 # Define variables and set up environment
 #############################################
-#root_fp = "/nfs/a319/gy17m2a/"
-root_fp = "C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/"
+root_fp = "/nfs/a319/gy17m2a/"
+#root_fp = "C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/"
 os.chdir(root_fp)
 
 # Create path to files containing functions
@@ -65,7 +65,7 @@ sys.path.insert(0, root_fp + 'Scripts/UKCP18/SpatialAnalyses')
 from Spatial_plotting_functions import *
 
 ems = ['01','04', '05', '06', '07', '08', '09','10','11','12', '13','15']
-years = range(1981,2000)  
+years = range(1981,2001)  
 n_highest=20
 #temp_perc_file='/nfs/a319/gy17m2a/Outputs/temp_stats_percentile.nc'
 
@@ -86,13 +86,13 @@ regional_gdf = regional_gdf.dissolve(by='merging_col')
 for em in ems:
     print(em)
     # Create dictionaries to store results
-    top_ten_dict = {}
+    #top_ten_dict = {}
     mean_dict = {}
     max_dict = {}
-    percentile1_dict ={}
-    percentile2_dict ={}
-    percentile3_dict ={}
-    percentile4_dict ={}
+    #percentile1_dict ={}
+    #percentile2_dict ={}
+    #percentile3_dict ={}
+    #percentile4_dict ={}
     # Loop through years
     for this_year in years:
         year=str(this_year)
@@ -132,80 +132,80 @@ for em in ems:
         #############################################   
         jja_mean = jja.aggregated_by(['season'], iris.analysis.MEAN)
         jja_max = jja.aggregated_by(['season'], iris.analysis.MAX)
-        jja_percentiles = jja.aggregated_by(['season'], iris.analysis.PERCENTILE, percent=[95,97,99,99.5])
-        percentile_1 = jja_percentiles[0,:,:,:]
-        percentile_2 = jja_percentiles[1,:,:,:]
-        percentile_3 = jja_percentiles[2,:,:,:]
-        percentile_4 = jja_percentiles[3,:,:,:]
+        #jja_percentiles = jja.aggregated_by(['season'], iris.analysis.PERCENTILE, percent=[95,97,99,99.5])
+        #percentile_1 = jja_percentiles[0,:,:,:]
+        #percentile_2 = jja_percentiles[1,:,:,:]
+        #percentile_3 = jja_percentiles[2,:,:,:]
+        #percentile_4 = jja_percentiles[3,:,:,:]
         
         mean_dict[year] = jja_mean.data.reshape(-1)
         max_dict[year] = jja_max.data.reshape(-1)
-        percentile1_dict[year] = percentile_1.data.reshape(-1)
-        percentile2_dict[year] = percentile_2.data.reshape(-1)
-        percentile3_dict[year] = percentile_3.data.reshape(-1)
-        percentile4_dict[year] = percentile_4.data.reshape(-1)
+        #percentile1_dict[year] = percentile_1.data.reshape(-1)
+        #percentile2_dict[year] = percentile_2.data.reshape(-1)
+        #percentile3_dict[year] = percentile_3.data.reshape(-1)
+        #percentile4_dict[year] = percentile_4.data.reshape(-1)
         
         #############################################
         # Find greatest N values
         #############################################   
-        rain_data=jja.data
+        #rain_data=jja.data
         # Make a conservative estimate (it is a bit annoying to deal with the rounding issues)
-        cutoff_percentile=100.*(1.0-(n_highest+1.0)/(np.shape(rain_data)[0]-1.0))
-        yearly_stats_percentile = jja.aggregated_by(['season'], iris.analysis.PERCENTILE, percent=cutoff_percentile)
+        #cutoff_percentile=100.*(1.0-(n_highest+1.0)/(np.shape(rain_data)[0]-1.0))
+        #yearly_stats_percentile = jja.aggregated_by(['season'], iris.analysis.PERCENTILE, percent=cutoff_percentile)
         
         ### Find top ten values
-        percentile_data=yearly_stats_percentile.data
+        #percentile_data=yearly_stats_percentile.data
         # Perform the main algorithm.
-        n_highest_array,exception=values_above_percentile(rain_data,percentile_data,n_highest)
-        if(exception==1):
-            raise Exception('The percent_data array has unexpected dimensions')
-        if(exception==2):
-            raise Exception('Cutoff percentile generates too few data points')
+        #n_highest_array,exception=values_above_percentile(rain_data,percentile_data,n_highest)
+        #if(exception==1):
+        #    raise Exception('The percent_data array has unexpected dimensions')
+        #if(exception==2):
+        #    raise Exception('Cutoff percentile generates too few data points')
          
         ######## Store in format for R, and add to dictionary
         # Remove Ensemble member dimension 
-        data = n_highest_array [0,:,:,:]           
+        #data = n_highest_array [0,:,:,:]           
         
         # Loops through each of the top ten values and store in a dictionary
         # with the year name
-        for i in range(0, data.shape[0]):
-            #print(i)
-            # Get data from one timeslice
-            one_ts = data[i,:,:]
-            # Extract data from one year 
-            one_ts = one_ts.reshape(-1)
-            # Store as dictionary with the year name
-            name = year + '_' + str(i)
-            top_ten_dict[name] = one_ts
+        #for i in range(0, data.shape[0]):
+        #    #print(i)
+        #    # Get data from one timeslice
+        #    one_ts = data[i,:,:]
+        #    # Extract data from one year 
+        #    one_ts = one_ts.reshape(-1)
+        #    # Store as dictionary with the year name
+        #    name = year + '_' + str(i)
+        #    top_ten_dict[name] = one_ts
             
     # Convert to dataframe
-    top_ten_df= pd.DataFrame(top_ten_dict)      
+    #top_ten_df= pd.DataFrame(top_ten_dict)      
     max_df = pd.DataFrame(max_dict)   
     mean_df = pd.DataFrame(mean_dict)   
-    percentile1_df = pd.DataFrame(percentile1_dict)   
-    percentile2_df = pd.DataFrame(percentile2_dict)   
-    percentile3_df = pd.DataFrame(percentile3_dict)   
-    percentile4_df = pd.DataFrame(percentile4_dict)   
+    #percentile1_df = pd.DataFrame(percentile1_dict)   
+    #percentile2_df = pd.DataFrame(percentile2_dict)   
+    #percentile3_df = pd.DataFrame(percentile3_dict)   
+    #percentile4_df = pd.DataFrame(percentile4_dict)   
     
     # Add lats and lons
     lats= jja.coord('latitude').points.reshape(-1)
     lons =  jja.coord('longitude').points.reshape(-1)   
-    top_ten_df['lat'], top_ten_df['lon'] = lats, lons
-    mean_df['lat'], mean_df['lon'] = lats, lons
-    max_df['lat'], max_df['lon'] = lats, lons
-    percentile1_df['lat'], percentile1_df['lon'] = lats, lons
-    percentile2_df['lat'], percentile2_df['lon'] = lats, lons
-    percentile3_df['lat'], percentile3_df['lon'] = lats, lons
-    percentile4_df['lat'], percentile4_df['lon'] = lats, lons
+    #top_ten_df['lat'], top_ten_df['lon'] = lats, lons
+    #mean_df['lat'], mean_df['lon'] = lats, lons
+    #max_df['lat'], max_df['lon'] = lats, lons
+    #percentile1_df['lat'], percentile1_df['lon'] = lats, lons
+    #percentile2_df['lat'], percentile2_df['lon'] = lats, lons
+    #percentile3_df['lat'], percentile3_df['lon'] = lats, lons
+    #percentile4_df['lat'], percentile4_df['lon'] = lats, lons
     
     # Save to file
-    top_ten_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/Greatest_twenty/em_{}.csv".format(em), index = False, float_format = '%.20f')
+    #top_ten_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/Greatest_twenty/em_{}.csv".format(em), index = False, float_format = '%.20f')
     max_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/Max/em_{}.csv".format(em), index = False, float_format = '%.20f')
     mean_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/Mean/em_{}.csv".format(em), index = False, float_format = '%.20f')
-    percentile1_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/95th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
-    percentile2_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/97th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
-    percentile3_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/99th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
-    percentile4_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/99.5th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
+    #percentile1_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/95th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
+    #percentile2_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/97th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
+    #percentile3_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/99th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
+    #percentile4_df.to_csv("Outputs/HiClimR_inputdata/NorthernSquareRegion/99.5th Percentile/em_{}.csv".format(em), index = False, float_format = '%.20f')
     
 
 
