3c6c950087132738804d2a58b057ca1495007b97
Molly Asher <gy17m2a@leeds.ac.uk>
2020-10-09 10:05:10 +0100

    improved efficiency of code

--- SpatialAnalyses/UK_stats_plotting.py
+++ SpatialAnalyses/UK_stats_plotting.py
@@ -12,9 +12,13 @@ import cartopy.crs as ccrs
 import matplotlib 
 import iris.plot as iplt
 import numpy.ma as ma
+import warnings
+warnings.simplefilter(action = 'ignore', category = FutureWarning)
 
 def namestr(obj, namespace):
     return [name for name in namespace if namespace[name] is obj]
+
+shared_axis = False
 
 ############################################
 # Define variables and set up environment
@@ -63,17 +67,14 @@ wider_northern_gdf = wider_northern_gdf[['geometry']]
 dg = gpd.read_file("datadir/SpatialData/2011_Census_Dumfries_and_Galloway_(shp)/DC_2011_EoR_Dumfries___Galloway.shp")
 dg['merging_col'] = 0
 dg = dg.dissolve(by='merging_col')
-dg.plot()
 borders = gpd.read_file('datadir/SpatialData/Scottish_Borders_shp/IZ_2001_EoR_Scottish_Borders.shp')
 borders['merging_col'] = 0
 borders = borders.dissolve(by='merging_col')
-borders.plot()
 
 southern_scotland = pd.concat([dg, borders])
 southern_scotland['new_merging_col'] = 0
 southern_scotland = southern_scotland.dissolve(by='new_merging_col')
 southern_scotland = southern_scotland[['geometry']]
-southern_scotland.plot()
 
 # Join the two
 wider_northern_gdf = pd.concat([southern_scotland, wider_northern_gdf])
@@ -81,165 +82,146 @@ wider_northern_gdf['merging_col'] = 0
 wider_northern_gdf = wider_northern_gdf.dissolve(by='merging_col')
 wider_northern_gdf = wider_northern_gdf.to_crs({'init' :'epsg:3785'}) 
 
-wider_northern_gdf.plot()
+# Load mask data
+mask = np.load('Outputs/RegionalMasks/wider_northern_region_mask.npy')
 
 ##################################################################
 # Create dictionaries storing the maximum and minimum values found for 
 # each statistic, considering all ensemble members and all grid cells
 ##################################################################
-# Create a dictionary to store results for each ensemble member (which is in itself a dictionary)
+
+# List of stats to loop through
+stats = ['jja_max', 'jja_mean', 'jja_p95', 'jja_p97', 'jja_p99', 'jja_p99.5', 'jja_p99.75', 'jja_p99.9']
+
+# Create a dictionary.
+# The keys will be ensemble member numbers and the values will be dictionarys
+# Each ensemble member's dictionary will in turn have statistic names as keys
+# and the cube of that statistic as values
 ems_dict = {}
+# Create dictionaries to store max and min values for each stat
+max_vals_dict = {}
+min_vals_dict = {}
 
 # Loop through ensemble members
 for em in ems:
     print(em)
     # Create dictionary for that ensemble member to store results of differnet stats
     em_dict = {}
-    # Load in netcdf files containing the max, mean and percentiles values over the whole UK
-    jja_max = iris.load('/nfs/a319/gy17m2a/Outputs/UK_stats_netcdf/em_'+ em+ '_jja_max.nc')[0]
-    jja_mean = iris.load('/nfs/a319/gy17m2a/Outputs/UK_stats_netcdf/em_'+ em+ '_jja_mean.nc')[0]
-    jja_percentiles = iris.load('/nfs/a319/gy17m2a/Outputs/UK_stats_netcdf/em_'+ em+ '_jja_percentiles.nc')[0]
-    
-    
-    # Trim to smaller area
-    jja_max = trim_to_bbox_of_region(jja_max, wider_northern_gdf)
-    jja_mean = trim_to_bbox_of_region(jja_mean, wider_northern_gdf)
-    jja_percentiles = trim_to_bbox_of_region(jja_percentiles, wider_northern_gdf)
-    
-    # List the different stats included
-    stats = [jja_mean, jja_max, jja_percentiles]
-    
-    # If this is the first time through the loop e.g. the first ensemble member, 
-    # then create dictionary which will store the max and min values for each 
-    # statistic across all the ensemble members
-    # Loop through the stats and for each set the value to sometihng unfeasible
-    if em == '01':
-        max_vals_dict = {}
-        min_vals_dict = {}
-        # Loop through stats setting max = 10000 and min = 0
-        for stat in stats:
-                name = namestr(stat, globals())[0]
-                max_vals_dict[name] = 0
-                min_vals_dict[name] = 10000
-        a
-    # Now loop through all the ensemble members and store the real values
+    # Loop through stats
     for stat in stats:
-        print(namestr(stat, globals())[0])
-        name = namestr(stat, globals())[0]
-        print(name)
-        print (stat.data.max()) 
-        em_dict[name] = stat
-        max_vals_dict[name] = stat.data.max() if stat.data.max() > max_vals_dict[name] else max_vals_dict[name]
-        min_vals_dict[name] = stat.data.min() if stat.data.min() < min_vals_dict[name] else min_vals_dict[name]      
-    
-    ems_dict[em] = em_dict       
-#
+          # Load in netcdf files containing the stats data over the whole UK
+          stat_cube = iris.load('/nfs/a319/gy17m2a/Outputs/UK_stats_netcdf/em_'+ em+ '_' + stat + '.nc')[0] 
+          # Trim to smaller area
+          stat_cube = trim_to_bbox_of_region(stat_cube, wider_northern_gdf)
+          # If this is the first time through the loop e.g. the first ensemble member, 
+          # then create dictionary which will store the max and min values for each 
+          # statistic across all the ensemble members
+          if em == '01':
+            # Loop through stats setting max = 10000 and min = 0
+            #name = namestr(stat, globals())[0]
+            max_vals_dict[stat] = 0
+            min_vals_dict[stat] = 10000
+          # For all ensemble members store the file in a dictionary
+          # And check if it's max/min value is higher/lower than the current value
+          # and store it if it is
+          print(stat)
+          print (stat_cube.data.max()) 
+          em_dict[stat] = stat_cube
+          max_vals_dict[stat] = stat_cube.data.max() if stat_cube.data.max() > max_vals_dict[stat] else max_vals_dict[stat]
+          min_vals_dict[stat] = stat_cube.data.min() if stat_cube.data.min() < min_vals_dict[stat] else min_vals_dict[stat]       
+          
+          # Save the dictionary of stat cubes to 
+          ems_dict[em] = em_dict         
+
 #############################################
 # Plotting
 #############################################
 # Create a colourmap                                   
 tol_precip_colors = ["#90C987", "#4EB256","#7BAFDE", "#6195CF", "#F7CB45", "#EE8026", "#DC050C", "#A5170E",
 "#72190E","#882E72","#000000"]                                      
-
 precip_colormap = matplotlib.colors.ListedColormap(tol_precip_colors)
 # Set the colour for any values which are outside the range designated in lvels
 precip_colormap.set_under(color="white")
 precip_colormap.set_over(color="white")
 
-stats = []
-for key, value in em_dict.items() :
-    stats.append(key)
+# stats = []
+# for key, value in em_dict.items() :
+#     stats.append(key)
+stats = ['jja_max', 'jja_mean', 'jja_p95', 'jja_p97', 'jja_p99', 'jja_p99.5', 'jja_p99.75', 'jja_p99.9']
 
 for stat in stats:
     print(stat)
-   # Extract the max, min values
-    #max_value = max_vals_dict[stat]
-    #min_value = min_vals_dict[stat]
-    
-    em_i=0
+    # Extract the max, min values
+    # For shared axis plotting
+    max_value = max_vals_dict[stat]
+    min_value = min_vals_dict[stat]
+    # Set up counters
     i=0
+    # Set up plot
     fig=plt.figure(figsize=(24,32))
     columns = 3
     rows = 4
+    
     for new_i in range(1, 13):
-            em = ems[em_i]
-            print(em)
+    for em in ems:
+        
+            
+            # Extract data for correct ensemble member and stat
+            # Remove time dimension (only had one value)
             em_dict = ems_dict[em]
-            # Extract correct stat
-            cube = em_dict[stat]
-           # print(cube)
+            stats_cube = em_dict[stat][0]
+                      
+            # Mask the data so as to cover any cells not within
+            # The wider northern region
+            stats_cube.data = ma.masked_where(mask == 0, stats_cube.data)
+            
+            # Trim to the BBOX of Northern England
+            # This ensures the plot shows nly the bbox around northern england
+            # but that all land values are plotted
+            stats_cube = trim_to_bbox_of_region(stats_cube, northern_gdf)
+
+            # Extract lats and lons for ploting and change projection
+            lats_2d = stats_cube.coord('latitude').points
+            lons_2d = stats_cube.coord('longitude').points
+            lons_2d, lats_2d = transform(Proj(init='epsg:4326'),Proj(init='epsg:3857'),lons_2d, lats_2d)
            
-            #Create a 2D grid
-            grid = cube[0]
-            
-            #grid = trim_to_bbox_of_region(grid, wider_northern_gdf)
-            #iplt.contourf(grid)
-            
-            # Mask out values outside SCotland, and below Midland and not land
-            #mask = mask_by_region(grid, wider_northern_gdf)
-            #np.save('mask.npy', mask)
-            mask = np.load('Outputs/RegionalMasks/wider_northern_region_mask.npy')
-            
-            # Masked data
-            masked_data = ma.masked_where(mask == 0, grid.data)
-            grid.data = masked_data
-            grid = trim_to_bbox_of_region(grid, northern_gdf)
-            #masked_data_1d = pd.DataFrame(masked_data.reshape(-1))
-            #masked_data_2d = masked
-            
-            # Trim to smaller area
-            #grid = trim_to_bbox_of_region(grid, test)
-            
-            lats_2d = grid.coord('latitude').points
-            lons_2d = grid.coord('longitude').points
-            inProj = Proj(init='epsg:4326')
-            outProj = Proj(init='epsg:3857')
-            lons_2d, lats_2d = transform(inProj,outProj,lons_2d, lats_2d)
-           
-            data=grid.data
-            #northern_grid = mask_by_region(grid, test)
-            
+            # CHecking plotting 
             #fig, ax = plt.subplots()
-            #my_plot = ax.pcolormesh(lons_2d, lats_2d, grid.data, linewidths=3, 
-            #                       alpha = 1, cmap = precip_colormap)
+            #my_plot = ax.pcolormesh(lons_2d, lats_2d, stats_cube.data, linewidths=3, 
+            #                      alpha = 1, cmap = precip_colormap)
             #northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
             
-            #ata = grid.data
-            #mask = northern_grid
-            #dat_test = ma.masked_where(mask == 0, data)
-            #dat_test_1d = dat_test.reshape(-1)
-                        
-            #data = northern_grid
-        
+            # Create subplot and plot
+            #(should we really find the corner coordinates?)
             ax = fig.add_subplot(rows, columns, new_i)
             ax.set_axis_off()
-            
-            ### Comparing plotting methods
-            #plt.contourf(lons_2d, lats_2d, data, linewidths=3, 
-            #                        alpha = 1, cmap = precip_colormap, levels = levels)
-            #plt.pcolormesh(lons_2d, lats_2d, data, linewidths=3, 
-            #                        alpha = 1, cmap = precip_colormap, vmin = min_value,
-            #                        vmax = max_value)
+            if shared_axis == True:
+                my_plot = ax.pcolormesh(lons_2d, lats_2d, stats_cube.data, linewidths=3, 
+                                        alpha = 1, cmap = precip_colormap,vmin = min_value,vmax = max_value)
+                leeds_gdf.plot(ax=ax, edgecolor='red', color='none', linewidth=3)
+                northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=3)
 
-            my_plot = ax.pcolormesh(lons_2d, lats_2d, grid.data, linewidths=3, 
-                                    alpha = 1, cmap = precip_colormap)
-                                    ,vmin = min_value,vmax = max_value)
-            leeds_gdf.plot(ax=ax, edgecolor='red', color='none', linewidth=3)
-            northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=3)
-            #fig.colorbar(my_plot, ax=ax, fraction=0.036, pad=0.02)
-            #cb1 = fig.colorbar(my_plot, ax=ax, fraction=0.16, pad=0.02)
-            #cb1.ax.tick_params(labelsize=40)
             
-            em_i = em_i +1
+            elif shared_axis == False:
+                my_plot = ax.pcolormesh(lons_2d, lats_2d, stats_cube.data, linewidths=3, 
+                                        alpha = 1, cmap = precip_colormap)
+                leeds_gdf.plot(ax=ax, edgecolor='red', color='none', linewidth=3)
+                northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=3)
+                #fig.colorbar(my_plot, ax=ax, fraction=0.036, pad=0.02)
+                cb1 = fig.colorbar(my_plot, ax=ax, fraction=0.16, pad=0.02)
+                cb1.ax.tick_params(labelsize=40)
+                        
+            # Move counter on to next ensemble member
+            i = i+1
         
     fig.tight_layout()
-    cbar_ax = fig.add_axes([1.05, 0.15, 0.05, 0.7])
-    cb1 = fig.colorbar(my_plot, cax=cbar_ax, fraction=0.36, pad=0.04)
-    cb1.ax.tick_params(labelsize=50)
-    filename = "Outputs/Stats_Spatial_plots/Northern/{}_diffscales.png".format(stat)
+    if shared_axis == True:
+        cbar_ax = fig.add_axes([1.05, 0.15, 0.05, 0.7])
+        cb1 = fig.colorbar(my_plot, cax=cbar_ax, fraction=0.36, pad=0.04)
+        cb1.ax.tick_params(labelsize=50)
+        filename = "Outputs/Stats_Spatial_plots/Northern/{}_diffscales.png".format(stat)
+    elif shared_axis == False:
+        filename = "Outputs/Stats_Spatial_plots/Northern/{}.png".format(stat)
     fig.savefig(filename, bbox_inches = 'tight')
-    
-
-
-
+   
 
