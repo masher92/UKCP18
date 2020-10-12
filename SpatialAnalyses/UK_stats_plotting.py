f6f13cef680258cb62fe3bdb12925d33230124b2
Molly Asher <gy17m2a@leeds.ac.uk>
2020-10-12 14:30:42 +0100

    updated so that colobar axes goes between smallest and largest values

--- SpatialAnalyses/UK_stats_plotting.py
+++ SpatialAnalyses/UK_stats_plotting.py
@@ -9,6 +9,7 @@ import numpy.ma as ma
 import warnings
 import iris.quickplot as qplt
 import iris.plot as iplt
+from matplotlib.colors import BoundaryNorm
 warnings.simplefilter(action = 'ignore', category = FutureWarning)
 
 def namestr(obj, namespace):
@@ -33,7 +34,7 @@ ems = ['01', '04', '05', '06', '07', '08', '09','10','11','12', '13','15']
 shared_axis = False
 
 # PLotting region
-region = 'leeds-at-centre'
+region = 'Northern'
 #region = ['Northern', 'leeds-at-centre', 'UK']
 
 ##################################################################
@@ -120,25 +121,35 @@ for stat in stats:
     print(stat)
     # Extract the max, min values
     # For shared axis plotting
-    max_value = max_vals_dict[stat]
-    min_value = min_vals_dict[stat]
-    # Set up counters
-    i=1
+    global_max = max_vals_dict[stat]
+    global_min = min_vals_dict[stat]
+    if max_value >10:
+        contour_levels_overall = np.linspace(global_min, global_max, 11,endpoint = True, dtype = int)
+    else:
+        contour_levels_overall = np.linspace(global_min, global_max, 11,endpoint = True)
+        contour_levels_overall = np.round(contour_levels_overall, 2) 
+        
     # Set up plot
     if region == 'Northern':
-        fig=plt.figure(figsize=(24,32))
+        plt.figure(figsize=(48,30), dpi=100)
     elif region == 'leeds-at-centre':
-        fig=plt.figure(figsize=(24,24))
-    columns = 3
-    rows = 4
-    
+        plt.figure(figsize=(48, 24), dpi=100)
+        
+    if shared_axis == True:# Also manually adjust the spacings which are used when creating subplots
+        plt.gcf().subplots_adjust(hspace=0.1, wspace=0.05, top=0.55, bottom=0.3, left=0.825, right=0.925)
+    if shared_axis == False:
+        plt.gcf().subplots_adjust(hspace=0.01, wspace=0.3, top=0.59, bottom=0.39, left=0.825, right=0.925)
+
+    # Set up counters
+    i=1
+
     #for new_i in range(1, 13):
     for em in ems:
         # Extract data for correct ensemble member and stat
         # Remove time dimension (only had one value)
         em_dict = ems_dict[em]
         stats_cube = em_dict[stat][0]
-                  
+           
         # Mask the data so as to cover any cells not within
         # The wider northern region
         if region == 'Northern':
@@ -147,171 +158,68 @@ for stat in stats:
             # This ensures the plot shows nly the bbox around northern england
             # but that all land values are plotted
             stats_cube = trim_to_bbox_of_region(stats_cube, northern_gdf)
-    
-        # Extract lats and lons for ploting and change projection
-        lats_2d = stats_cube.coord('latitude').points
-        lons_2d = stats_cube.coord('longitude').points
-        lons_2d, lats_2d = transform(Proj(init='epsg:4326'),Proj(init='epsg:3857'),lons_2d, lats_2d)
-         
-        ax = plt.gca()
         
-        
-        #lats_2d, lons_2d =find_cornerpoint_coordinates(stats_cube)   
-        #stats_cube = stats_cube[0:32,0:36]
-
-        # CHecking plotting 
-        
-        qplt.pcolormesh(stats_cube)
-        qplt.contourf(stats_cube)
-        
-        fig, ax = plt.subplots()
-        my_plot = ax.pcolormesh(lons_2d, lats_2d, stats_cube.data, linewidths=3)
-                             # alpha = 1, cmap = precip_colormap, vmin =0, vmax = 175)
-        leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
-        #northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
-        fig.colorbar(my_plot, ax=ax, fraction=0.06, pad=0.02)
-        
-        fig1 = plt.figure()
-        ax = plt.gca()
-       # ax= fig1.add_subplot(2,1,1)
-        #fig, ax = plt.subplots()
-        qplt.contourf(stats_cube, axes=ax)
-        
-                             # alpha = 1, cmap = precip_colormap, vmin =0, vmax = 175)
-        leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
-        #northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
-        fig.colorbar(my_plot, ax=ax, fraction=0.06, pad=0.02)
-        
-        proj = ccrs.Mercator.GOOGLE
-        ax = plt.subplot(122, projection=proj)
-        qplt.pcolormesh(stats_cube, 25)
-        #iplt.pcolormesh(stats_cube,cmap = precip_colormap, 25)
-        leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
-        #plt.gca().coastlines()
-        plt.show()
-        
-        ######################### 
-        # Think this is what is needed
-        # Need to work out subplotting
-        plt.axes(projection=ccrs.Mercator.GOOGLE)
-        # Make a pseudocolour plot using this colour scheme.
-        mesh = iplt.pcolormesh(stats_cube, cmap=precip_colormap)
-        # Add a colourbar, with extensions to show handling of out-of-range values.
-        bar = plt.colorbar(mesh, orientation='vertical', extend='both')
-        leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)        
-        
-        proj = ccrs.Mercator.GOOGLE
-        ax = plt.subplot(122, projection=proj)
-        mesh = iplt.pcolormesh(stats_cube)
-        #iplt.pcolormesh(stats_cube,cmap = precip_colormap, 25)
-        leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
-        #cb1 = fig.colorbar(mesh, ax=ax, fraction=0.06, pad=0.02)
-        #cb1.ax.tick_params(labelsize=20)
-        #bar = plt.colorbar(mesh, orientation='vertical', extend='both')
-        #cbar_ax = fig.add_axes([1, 0.15, 0.035, 0.7])
-        #cb1 = fig.colorbar(mesh, cax=cbar_ax, fraction=0.36, pad=0.04)
-        #cb1.ax.tick_params(labelsize=20)
-        plt.show()
-        
-        # Create subplot and plot
-        #(should we really find the corner coordinates?)
-        ax = fig.add_subplot(rows, columns, i)
-        ax.set_axis_off()
+        if stats_cube.data.max() > 10:
+            local_min = int(stats_cube.data.min())
+            local_max = int(stats_cube.data.max())
+            contour_levels = np.linspace(local_min, local_max, 5,endpoint = True, dtype = int)
+        else:
+            local_min = round(stats_cube.data.min(),2)
+            local_max = round(stats_cube.data.max(),2)
+            contour_levels = np.linspace(local_min, local_max, 5,endpoint = True)
+            contour_levels = np.round(contour_levels, 2) 
+        #v = np.linspace(local_min, local_max, 5,endpoint = True)
+        print(local_max)
+        # Make 50 evenly spaced levels which span the dataset
+        #norm = BoundaryNorm(contour_levels, ncolors=precip_colormap.N, clip = True)       
+       
         if shared_axis == True:
-            my_plot = ax.pcolormesh(lons_2d, lats_2d, stats_cube.data, linewidths=3, 
-                                    alpha = 1, cmap = precip_colormap,vmin = min_value,vmax = max_value)
-            if region == 'Northern':
-                leeds_gdf.plot(ax=ax, edgecolor='red', color='none', linewidth=3)
-                northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=3)
-            elif region == 'leeds-at-centre':
-                leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=3)
-            
+           proj = ccrs.Mercator.GOOGLE
+           ax = plt.subplot(4,3,i, projection=proj)
+           mesh = iplt.pcolormesh(stats_cube, cmap = precip_colormap, vmin = global_min,
+                                  vmax = global_max)
+           if region == 'Northern':
+                leeds_gdf.plot(ax=ax, edgecolor='red', color='none', linewidth=1)
+                northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
+           elif region == 'leeds-at-centre':
+                leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
+                
         elif shared_axis == False:
-            my_plot = ax.pcolormesh(lons_2d, lats_2d, stats_cube.data, linewidths=3, 
-                                    alpha = 1, cmap = precip_colormap)
-            if region == 'Northern':
-                leeds_gdf.plot(ax=ax, edgecolor='red', color='none', linewidth=3)
-                northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=3)
-            elif region == 'leeds-at-centre':
-                leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=3)
-            #fig.colorbar(my_plot, ax=ax, fraction=0.036, pad=0.02)
-            cb1 = fig.colorbar(my_plot, ax=ax, fraction=0.06, pad=0.02)
-            cb1.ax.tick_params(labelsize=40)
+           proj = ccrs.Mercator.GOOGLE
+           ax = plt.subplot(4,3,i, projection=proj)
+           mesh = iplt.pcolormesh(stats_cube, cmap = precip_colormap, vmin = local_min, 
+                                  vmax = local_max)
+           #iplt.pcolormesh(stats_cube,cmap = precip_colormap, 25)
+           if region == 'Northern':
+                leeds_gdf.plot(ax=ax, edgecolor='red', color='none', linewidth=1)
+                northern_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
+                cb1 = plt.colorbar(mesh, ax=ax, fraction=0.051, pad=0.03, 
+                                  boundaries = contour_levels)
+           elif region == 'leeds-at-centre':
+                leeds_gdf.plot(ax=ax, edgecolor='black', color='none', linewidth=1)
+                cb1 = plt.colorbar(mesh, ax=ax, fraction=0.041, pad=0.03, 
+                                  boundaries = contour_levels)
+           #cb1.ax.set_xticklabels(["{:.0}".format(i) for i in colorbar.get_ticks()])
+           cb1.ax.tick_params(labelsize=8)
+           cb1.update_ticks()
                     
         # Move counter on to next ensemble member
         i = i+1
       
     # Set up plottingparameters over whole subfigure
-    fig.tight_layout()
+  
+    # make an axes to put the shared colorbar in
+    # 1,2 are coordinates of lower left corner of plot; 3,4 are width and height of subplot
+    #colorbar_axes = plt.gcf().add_axes([0.825, 0.275, 0.1, 0.019])
+    #colorbar = plt.colorbar(mesh, colorbar_axes, orientation='horizontal')
     if shared_axis == True:
-        cbar_ax = fig.add_axes([0.99, 0.15, 0.035, 0.7])
-        #cbar_ax = fig.add_axes([1.05, 0.15, 0.05, 0.7])
-        cb1 = fig.colorbar(my_plot, cax=cbar_ax, fraction=0.36, pad=0.04)
-        cb1.ax.tick_params(labelsize=50)
+        colorbar_axes = plt.gcf().add_axes([0.927, 0.3, 0.005, 0.25])
+        colorbar = plt.colorbar(mesh, colorbar_axes, orientation='vertical', 
+                                boundaries = contour_levels_overall)  
+        colorbar.set_label('%s' % stats_cube.units, size = 15)
+        colorbar.ax.tick_params(labelsize=15)
+        colorbar.ax.set_xticklabels(["{:.2}".format(i) for i in colorbar.get_ticks()]) # set ticks of your format     
         filename = "Outputs/Stats_Spatial_plots/{}/{}.png".format(region, stat)
     elif shared_axis == False:
         filename = "Outputs/Stats_Spatial_plots/{}/{}_diffscales.png".format(region, stat)
-    fig.savefig(filename, bbox_inches = 'tight')
-   
-def main():
-    # extract surface temperature cubes which have an ensemble member coordinate, adding appropriate lagged ensemble metadata
-    surface_temp = iris.load_strict(iris.sample_data_path('GloSea4', 'ensemble_???.pp'),
-                  iris.Constraint('surface_temperature', realization=lambda value: True),
-                  callback=realization_metadata,
-                  )
-
- # ----------------------------------------------------------------------------------------------------------------
-    # Plot #1: Ensemble postage stamps
-    # ----------------------------------------------------------------------------------------------------------------
-
-    # for the purposes of this example, take the last time element of the cube
-    last_timestep = surface_temp[:, -1, :, :]
-
-    # Make 50 evenly spaced levels which span the dataset
-    contour_levels = numpy.linspace(numpy.min(last_timestep.data), numpy.max(last_timestep.data), 50)
-
-    # Create a wider than normal figure to support our many plots
-    plt.figure(figsize=(12, 6), dpi=100)
-
-    # Also manually adjust the spacings which are used when creating subplots
-    plt.gcf().subplots_adjust(hspace=0.05, wspace=0.05, top=0.95, bottom=0.05, left=0.075, right=0.925)
-
-    # iterate over all possible latitude longitude slices
-    for cube in stats_cube.slices(['grid_latitude', 'grid_longitude']):
-        print(cube)
-        
-        # get the ensemble member number from the ensemble coordinate
-        ens_member = cube.coord('realization').points[0]
-
-        # plot the data in a 4x4 grid, with each plot's position in the grid being determined by ensemble member number
-        # the special case for the 13th ensemble member is to have the plot at the bottom right
-        if ens_member == 13:
-            plt.subplot(4, 4, 16)
-        else:
-            plt.subplot(4, 4, ens_member+1)
-
-        cf = iplt.contourf(cube, contour_levels)
-
-        # add coastlines
-        m = iplt.gcm()
-        m.drawcoastlines()
-
-    # make an axes to put the shared colorbar in
-    colorbar_axes = plt.gcf().add_axes([0.35, 0.1, 0.3, 0.05])
-    colorbar = plt.colorbar(cf, colorbar_axes, orientation='horizontal')
-    colorbar.set_label('%s' % last_timestep.units)
-
-    # limit the colorbar to 8 tick marks
-    import matplotlib.ticker
-    colorbar.locator = matplotlib.ticker.MaxNLocator(8)
-    colorbar.update_ticks()
-
-    # get the time for the entire plot
-    time_coord = last_timestep.coord('time')
-    time = time_coord.units.num2date(time_coord.points[0])
-
-    # set a global title for the postage stamps with the date formated by "monthname year"
-    plt.suptitle('Surface temperature ensemble forecasts for %s' % time.strftime('%B %Y'))
-
-    iplt.show()
-
+    plt.savefig(filename, bbox_inches = 'tight')
