713d5a71640eda19f4563a1096dabbf61638a688
Molly Asher <gy17m2a@leeds.ac.uk>
2020-10-19 09:52:24 +0100

    think without dissolving the GDF it doesn't work properly as a mask

--- SpatialAnalyses/CreatingMask.py
+++ SpatialAnalyses/CreatingMask.py
@@ -27,24 +27,29 @@ sys.path.insert(0, root_fp + 'Scripts/UKCP18/')
 from Pr_functions import *
 sys.path.insert(0, root_fp + 'Scripts/UKCP18/SpatialAnalyses')
 from Spatial_plotting_functions import *
+from Spatial_geometry_functions import *
 
 # Load required geodataframes
 #wider_northern_gdf = create_wider_northern_outline({'init' :'epsg:3857'})
 uk_gdf= create_uk_outline({'init' :'epsg:3857'})
+#uk_gdf = create_leeds_at_centre_outline({'init' :'epsg:3857'})
 
 # Load a cube
-jja_max = iris.load('/nfs/a319/gy17m2a/Outputs/UK_stats_netcdf/em_'+ em+ '_jja_max.nc')[0]
+jja_max = iris.load('/nfs/a319/gy17m2a/Outputs/UK_stats_netcdf/Allhours/em_'+ em+ '_jja_max.nc')[0]
 #jja_max = trim_to_bbox_of_region(jja_max, wider_northern_gdf)
 grid = jja_max[0]
+print(grid)
 #iplt.contourf(grid)
 #plt.gca().coastlines()
 
 # Create the mask
 #mask = mask_by_region(grid, wider_northern_gdf)
 # Create the mask
+seconds = time.time()
 uk_mask = mask_by_region(grid, uk_gdf)
+print("Loaded data in ", time.time() - seconds)
 
 # Save the numpy array 
 #np.save('Outputs/RegionalMasks/wider_northern_region_mask.npy', mask)
 # Save the numpy array 
-np.save('Outputs/RegionalMasks/uk_mask.npy', mask)
+np.save('Outputs/RegionalMasks/uk_mask_new.npy', uk_mask)
