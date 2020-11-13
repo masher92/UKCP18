4a059e68cf62926dacd003f05532fab88311b82c
Molly Asher <gy17m2a@leeds.ac.uk>
2020-11-13 15:48:56 +0000

    updating PDF ploting functions so they can work with dictionaries containing variables numbers of entries

--- PointLocationStats/PlotPDFs/PDF_plotting_functions.py
+++ PointLocationStats/PlotPDFs/PDF_plotting_functions.py
@@ -42,28 +42,26 @@ def log_discrete_bins(min_value,max_value,bins_if_log_spaced,discretisation):
 ########################################################
 ### Plotting functions
 ########################################################
-#navy = mpatches.Patch(color='navy', label='Observations')
-#firebrick = mpatches.Patch(color='firebrick', label='Projections')
 
-def equal_spaced_histogram (results_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
-    keys = list(results_dict.keys())
-    navy = mpatches.Patch(color='navy', label=str(keys[0]))
-    firebrick = mpatches.Patch(color='firebrick', label=str(keys[1]))
-
+def equal_spaced_histogram (results_dict, cols_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
+   
+    patches= []
     for key, df in results_dict.items():
+        print(key)
+        col = cols_dict[key]
+        patch = mpatches.Patch(color=col, label=key)
+        patches.append(patch)
+        
         # Create a histogram and save the bin edges and the values in each bin
         values, bin_edges = np.histogram(df['Precipitation (mm/hr)'], bins=bin_nos, density=True)
         # Calculate the bin central positions
         bin_centres =  0.5*(bin_edges[1:] + bin_edges[:-1])
         # Draw the plot
-        if key == keys[0]:
-            plt.plot(bin_centres, values,linewidth = 1, color = 'navy')
-        elif key == keys[1]:
-            plt.plot(bin_centres, values,  linewidth = 1, color = 'firebrick')
+        plt.plot(bin_centres, values,linewidth = 1, color = cols_dict[key])
         #plt.plot(bin_centres, values, color='black', marker='o',markersize =1, linewidth=0.5, markerfacecolor = 'red')
         #plt.hist(wethours['Precipitation (mm/hr)'], bins = bin_no, density = True, color = 'white', edgecolor = 'black', linewidth= 0.5)
-    
-    plt.legend(handles=[navy, firebrick])
+        
+    plt.legend(handles=patches)
     plt.xlabel('Precipitation (mm/hr)')
     plt.ylabel('Probability density')
     plt.title(str(bin_nos) + " bins")
@@ -72,50 +70,57 @@ def equal_spaced_histogram (results_dict, bin_nos, x_axis_scaling = 'linear', y_
    
 
 # Plot with log spaced bins (Holloway, 2012)
-def log_spaced_histogram(results_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
-    keys = list(results_dict.keys())
-    navy = mpatches.Patch(color='navy', label=str(keys[0]))
-    firebrick = mpatches.Patch(color='firebrick', label=str(keys[1]))
+def log_spaced_histogram(results_dict, cols_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
     
     # Find maximum and minimum values across all dataframes in dictionary
     min_value = find_min_max_dict_values(results_dict)[0]
     max_value = find_min_max_dict_values(results_dict)[1]
     
+    patches= []
     for key, df in results_dict.items():
+        # Define the colour to use for this entry
+        # Create a patch for this colour to be used in creating the legend
+        # And add to list of patches for use in legend
+        col = cols_dict[key]
+        patch = mpatches.Patch(color=col, label=key)
+        patches.append(patch)
+        
         # Create logarithmically spaced bins
         # Need to go slightly under the number e.g. 0.2 otherwise it excluded values of exactly 0.2
-        #bins = 10 ** np.linspace(np.log10(0.1), np.log10(30), 50)
-        bins = np.logspace(np.log10(min_value-0.01),np.log10(max_value), bin_nos)  
+        bins = 10 ** np.linspace(np.log10(0.1), np.log10(30), 50)
+        #bins = np.logspace(np.log10(min_value-0.01),np.log10(max_value), bin_nos)  
             
         # Use in built np.histogram density calculator to count numbers in each bin
         density, bin_edges = np.histogram(df['Precipitation (mm/hr)'], bins= bins, density=True)
         bin_centres =  0.5*(bin_edges[1:] + bin_edges[:-1])  
         # Draw the plot
-        if key == keys[0]:
-            plt.plot(bin_centres, density ,linewidth = 1, color = 'navy')
-        elif key == keys[1]:
-            plt.plot(bin_centres, density,  linewidth = 1, color = 'firebrick')
+        plt.plot(bin_centres, density ,linewidth = 1, color = cols_dict[key])
         
-    plt.legend(handles=[navy, firebrick])
+    plt.legend(handles=patches)
     plt.xlabel('Precipitation (mm/hr)')
     plt.ylabel('Probability density')
     plt.title(str(bin_nos) + " bins")
     plt.xscale(x_axis_scaling)
     plt.yscale(y_axis_scaling)  
-        
     
-def fractional_contribution(results_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
-    keys = list(results_dict.keys())
-    navy = mpatches.Patch(color='navy', label=str(keys[0]))
-    firebrick = mpatches.Patch(color='firebrick', label=str(keys[1]))
+def fractional_contribution(results_dict, cols_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
     
     # Find maximum and minimum values across all dataframes in dictionary
     min_value = find_min_max_dict_values(results_dict)[0]
     max_value = find_min_max_dict_values(results_dict)[1]
     
+    patches= []
     for key, df in results_dict.items():
+        # Define the colour to use for this entry
+        # Create a patch for this colour to be used in creating the legend
+        # And add to list of patches for use in legend
+        col = cols_dict[key]
+        patch = mpatches.Patch(color=col, label=key)
+        patches.append(patch)
+        
         # Create log spaced bins
-        bins = np.logspace(np.log10(min_value-0.01),np.log10(max_value), bin_nos)  
+        #bins = np.logspace(np.log10(min_value-0.01),np.log10(max_value), bin_nos)  
+        bins = np.logspace(np.log10(0.1),np.log10(max_value), bin_nos) 
         # Find the numbers of precipitation measurements in each bin   
         freqs, bin_edges = np.histogram(df['Precipitation (mm/hr)'], bins= bins, density=False)
         # Find the centre point of each bin for plotting
@@ -134,12 +139,9 @@ def fractional_contribution(results_dict, bin_nos, x_axis_scaling = 'linear', y_
             # Add values to list
             fcs.append(fc)
         # Draw the plot
-        if key == keys[0]:
-            plt.plot(bin_centres, fcs ,linewidth = 1, color = 'navy')
-        elif key == keys[1]:
-            plt.plot(bin_centres, fcs,  linewidth = 1, color = 'firebrick')
+        plt.plot(bin_centres, freqs ,linewidth = 1, color = cols_dict[key])
         
-    plt.legend(handles=[navy, firebrick])
+    plt.legend(handles=patches)
     plt.xlabel('Precipitation (mm/hr)')
     plt.ylabel('Fractional contribution to rainfall')
     plt.title(str(bin_nos) + " bins")
@@ -147,13 +149,10 @@ def fractional_contribution(results_dict, bin_nos, x_axis_scaling = 'linear', y_
     plt.yscale(y_axis_scaling)
         
         
-def log_discrete_histogram(results_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
-    keys = list(results_dict.keys())
-    navy = mpatches.Patch(color='navy', label=str(keys[0]))
-    firebrick = mpatches.Patch(color='firebrick', label=str(keys[1]))
+def log_discrete_histogram(results_dict, cols_dict, bin_nos, x_axis_scaling = 'linear', y_axis_scaling = 'linear'):
     
     # Create bin edges based on data in all of the dataframes, i.e. use the same bin edges for all dataframes
-    #min_value = find_min_max_dict_values(dict)[0]
+    min_value = find_min_max_dict_values(results_dict)[0]
     max_value = find_min_max_dict_values(results_dict)[1]
 
     # Maybe min value shoudl be set at 0.05 to make the spacings at the right place
@@ -165,33 +164,37 @@ def log_discrete_histogram(results_dict, bin_nos, x_axis_scaling = 'linear', y_a
     bin_edges=log_discrete_bins(min_value,max_value,bins_if_log_spaced,discretisation)
     #print ("Based on " + str(bins_if_log_spaced) + " log spaced bins, " + str(len(bin_edges)) + " bins created with " + str(min_value) + str (max_value))
    
+    patches= []    
     fig, ax = plt.subplots()
     for key, df in results_dict.items():
+        # Define the colour to use for this entry
+        # Create a patch for this colour to be used in creating the legend
+        # And add to list of patches for use in legend
+        col = cols_dict[key]
+        patch = mpatches.Patch(color=col, label=key)
+        patches.append(patch)
+        
         # Find the numbers of precipitation measurements in each bin   
-        densities, bin_edges = np.histogram(df['Precipitation (mm/hr)'], bins= bin_edges, density=True)
+        freqs, bin_edges = np.histogram(df['Precipitation (mm/hr)'], bins= bin_edges, density=True)
         # Find the centre point of each bin for plotting
         bin_centres =  0.5*(bin_edges[1:] + bin_edges[:-1])    
-        # Plot
-       # plt.plot(bin_centres, densities, linewidth = 1.5, label = key)
         # Draw the plot
-        if key == keys[0]:
-            plt.plot(bin_centres, densities ,linewidth = 1, color = 'navy')
-        elif key == keys[1]:
-            plt.plot(bin_centres, densities,  linewidth = 1, color = 'firebrick')
+        plt.plot(bin_centres, freqs ,linewidth = 1, color = cols_dict[key])
 
-
-    plt.legend(handles=[navy, firebrick])
+    plt.legend(handles=patches)
     plt.xlabel('Precipitation (mm/hr)')
     plt.ylabel('Probability density')
     plt.title(str(len(bin_edges)) + " bins")
     plt.xscale(x_axis_scaling)
     plt.yscale(y_axis_scaling)
+    #formatter = FormatStrFormatter('%.3f')
+    #ax.yaxis.set_major_formatter(formatter)
         
     # Remove scientific notation from y-axis
-    for axis in [ax.yaxis]:
-        formatter = ScalarFormatter()
-        formatter.set_scientific(False)
-        ax.yaxis.set_major_formatter(formatter)
+    # for axis in [ax.yaxis]:
+    #     formatter = ScalarFormatter()
+    #     formatter.set_scientific(False)
+    #     ax.yaxis.set_major_formatter(formatter)
     
         
 ## Holloway method manually applied
