28ffb80d713d81a22d5aa9922943f58e3036d005
Molly Asher <gy17m2a@leeds.ac.uk>
2021-02-12 15:43:20 +0000

    now works with reformatted ceh_gear data, and the plotting to check data from the correct location has been extracted works!

--- PointLocationStats/CreateTimeSeries/CreateTimeSeries_obs.py
+++ PointLocationStats/CreateTimeSeries/CreateTimeSeries_obs.py
@@ -34,73 +34,104 @@ os.chdir(root_fp)
 
 sys.path.insert(0, root_fp + 'Scripts/UKCP18/GlobalFunctions')
 from Obs_functions import *
+from Spatial_plotting_functions import *
+from Spatial_geometry_functions import *
 
 # Define name and coordinates of location
 location = 'Armley'
-lat = -1.37818
-lon = 53.79282
+lat = 53.79282
+lon = -1.37818
+
+#############################################################################
+# Load in spatial data
+# As geodataframes for plotting
+# As shapely geometries for checking whether lat/long points are witihn the areas
+#############################################################################
+# This is the outline of Leeds
+leeds_gdf = create_leeds_outline({'init' :'epsg:3857'})
+# This is a square area surrounding Leeds
+leeds_at_centre_gdf = create_leeds_at_centre_outline({'init' :'epsg:3857'})
+
+#############################################################################
+## Load in the reformatted observations cubes 
+# and concatenate into one cube
+#############################################################################
+filenames =[]
+# Create filepath to correct folder using ensemble member and year
+general_filename = 'Outputs/RegriddingObservations/CEH-GEAR_reformatted/rf_*'
+# Find all files in directory which start with this string
+for filename in glob.glob(general_filename):
+    #print(filename)
+    filenames.append(filename)
+    print(len(filenames))
+# Load all cubes into list
+monthly_cubes_list = iris.load(filenames,'rainfall_amount')
+
+# Concatenate the cubes into one
+concat_cube = monthly_cubes_list.concatenate_cube()
+
+# Test plotting
+iplt.pcolormesh(concat_cube[12])
+
+#############################################################################
+## Trim concatenated cube to outline of leeds-at-centre geodataframe
+#############################################################################
+concat_cube = trim_to_bbox_of_region_obs(concat_cube, leeds_at_centre_gdf)
+
+# Test plotting
+iplt.pcolormesh(concat_cube[12])
 
 ############################################################
-# Read in monthly cubes and concatenate into one long timeseries cube
-###########################################################
-# For some reason a backslash was showing instead of a forward slash
-filenames = [os.path.normpath(i) for i in glob.glob('datadir/CEH-GEAR/CEH-GEAR-1hr_*')]
+# Create time series cube at this location, and return the associated   
+############################################################
+# Create time series cube at this location, and return the associated         
+result = create_concat_cube_one_location_obs(concat_cube, lat, lon)
 
-# Load in the rainfall cubes into a list, taking just the rainfall amount
-monthly_cubes_list = iris.load(filenames, 'rainfall_amount')
-
-# Concatenate the cubes
-obs_cubes = monthly_cubes_list.concatenate_cube()
-
-##############################################################################
-# Extract the indices of the data point in the cube closest to a point of interest
-##############################################################################
-# Read in the list of cubes, containing the lat and long cubes
-one_cube = iris.load(filenames[0])
-
-# Find the index of the point in the grid which is closest to the point of interest
-# Considering the grid both flipped and not flipped
-# For just creating a time series and not plotting, flipping is not really necesary
-# but can be used in the testing below
-closest_idx = find_idx_closestpoint(one_cube, lat, lon, flip = False)
-closest_idx_fl = find_idx_closestpoint(one_cube, lat, lon, flip = True)
+# Split out results
+time_series_cube, closest_lat, closest_long, closest_point_idx = result[0], result[1], result[2], result[3]
 
 #############################################################################
-# Check that the index returned by this process matches expected location.
-# Use one hour of data to get the shape of the data.
-# Set all values to 0; expect for at the location found above.
-# Plot and check location
-##############################################################################
-# hour = obs_cubes[1]
-# #Extract the data
-# hour_data = hour.data
-# # Flip the data so it's not upside down
-# hour_data_fl = np.flipud(hour_data)
-# # Fill empty values with NaN
-# hour_data_fl = hour_data_fl.filled(np.nan) 
-# # Fill all places with 0
-# hour_data_fl.fill(0)
-# # Fill the location with a different value
-# hour_data_fl[closest_idx_fl[0],closest_idx_fl[1]] = 7
+## Check that the data has been extracted for the correct location
+# By creating a cube in which all the data values have been masked out
+# except from at the grid cell identified by closest_point_idx
+#############################################################################
+# Create cube with all values masked out except from cell at closest_point_idx
+cube_higlighted_cell = create_grid_highlighted_cell(concat_cube, closest_point_idx)
+cube_higlighted_cell_data = cube_higlighted_cell.data
 
-# # # Plot
-# contour = plt.contourf(hour_data_fl)
-# contour = plt.colorbar()
-# contour =plt.axes().set_aspect('equal') 
-# plt.plot(closest_idx_fl[1], closest_idx_fl[0], 'o', color='black', markersize = 3) 
+# Find cornerpoint coordinates (for use in plotting)
+lats_cornerpoints = find_cornerpoint_coordinates_obs(cube_higlighted_cell)[0]
+lons_cornerpoints = find_cornerpoint_coordinates_obs(cube_higlighted_cell)[1]
+
+# Trim the data timeslice to be the same dimensions as the corner coordinates
+cube_higlighted_cell_data = cube_higlighted_cell_data[1:,1:]
 
 #############################################################################
-# Trim the concatenated cube to the location of interest
+#### # Plot - highlighting grid cells whose centre point falls within Leeds
+# Uses the lats and lons of the corner points but with the values derived from 
+# the associated centre point
 ##############################################################################
-# Keep all of the first dimension (time), and trim to just the location of interest
-interpolated_cube = obs_cubes[:,closest_idx[0], closest_idx[1]]
+# Create location in web mercator for plotting
+lon_wm,lat_wm = transform(Proj(init = 'epsg:4326') , Proj(init = 'epsg:3857') , lon, lat)
 
-# Plot the timeseries
-# qplt.plot(interpolated_cube)
-# plt.xticks(rotation=45)
+# Create a colormap
+cmap = mpl.colors.ListedColormap(['yellow'])
 
-# iplt.plot(obs_cubes)
-# plt.xticks(rotation=45)
+fig, ax = plt.subplots(figsize=(20,10))
+extent = tilemapbase.extent_from_frame(leeds_gdf)
+plot = plotter = tilemapbase.Plotter(extent, tilemapbase.tiles.build_OSM(), width=600)
+plot =plotter.plot(ax)
+# Add edgecolor = 'grey' for lines
+plot =ax.pcolormesh(lons_cornerpoints, lats_cornerpoints, cube_higlighted_cell_data,
+      linewidths=0.4, alpha = 1, cmap = cmap, edgecolors = 'grey')
+plot = ax.xaxis.set_major_formatter(plt.NullFormatter())
+plot = ax.yaxis.set_major_formatter(plt.NullFormatter())
+plot =leeds_gdf.plot(ax=ax, categorical=True, alpha=1, edgecolor='black', color='none', linewidth=2)
+plot =leeds_at_centre_gdf.plot(ax=ax, categorical=True, alpha=1, edgecolor='black', color='none', linewidth=2)
+plt.plot(lon_wm, lat_wm,  'o', color='black', markersize = 2)     
+#plt.savefig('Scripts/UKCP18/RainGaugeAnalysis/Figs/NewcastleGaugeGridCells/{}.png'.format(station_name))
+plt.show()
+   
 
 #############################################################################
 # Cut to time period matching up with UKCP18 data
@@ -109,27 +140,27 @@ interpolated_cube = obs_cubes[:,closest_idx[0], closest_idx[1]]
 days_constraint = iris.Constraint(time=lambda cell: PartialDateTime(year = 1990, month=1, day=11) < cell.point < PartialDateTime(year = 2001, month=1, day=1))
 
 # Trim data to this time period
-interpolated_cube_1990_2001 = interpolated_cube.extract(days_constraint)
+time_series_cube_1990_2001 = time_series_cube.extract(days_constraint)
 
 ############################################################################
 # Create as dataframe
 ##############################################################################
 # Create a dataframe containing the date and the precipitation data
-df = pd.DataFrame({'Date': np.array(interpolated_cube.coord('time').points),
-                  'Precipitation (mm/hr)': np.array(interpolated_cube.data),
-                  'Date_formatted': interpolated_cube.coord('time').units.num2date(interpolated_cube.coord('time').points)})
+df = pd.DataFrame({'Date': np.array(time_series_cube.coord('time').points),
+                  'Precipitation (mm/hr)': np.array(time_series_cube.data),
+                  'Date_formatted': time_series_cube.coord('time').units.num2date(time_series_cube.coord('time').points)})
 
 # Create a dataframe containing the date and the precipitation data
-df_1990_2001 = pd.DataFrame({'Date': np.array(interpolated_cube_1990_2001.coord('time').points),
-                  'Precipitation (mm/hr)': np.array(interpolated_cube_1990_2001.data),
-                  'Date_formatted': interpolated_cube_1990_2001.coord('time').units.num2date(interpolated_cube_1990_2001.coord('time').points)})
+df_1990_2001 = pd.DataFrame({'Date': np.array(time_series_cube_1990_2001.coord('time').points),
+                  'Precipitation (mm/hr)': np.array(time_series_cube_1990_2001.data),
+                  'Date_formatted': time_series_cube_1990_2001.coord('time').units.num2date(time_series_cube_1990_2001.coord('time').points)})
 
 ###########################################################
 # Save cube and csv
 ###########################################################
-iris.save(interpolated_cube, 
+iris.save(time_series_cube, 
           "/nfs/a319/gy17m2a/Outputs/TimeSeries/CEH-GEAR/Armley/TimeSeries_cubes/1990-2014.nc")
-iris.save(interpolated_cube_1990_2001, 
+iris.save(time_series_cube_1990_2001, 
           "/nfs/a319/gy17m2a/Outputs/TimeSeries/CEH-GEAR/Armley/TimeSeries_cubes/1990-2001.nc")
 
 df.to_csv("/nfs/a319/gy17m2a/Outputs/TimeSeries/CEH-GEAR/Armley/TimeSeries_csv/1990-2014.csv", index = False)
@@ -138,6 +169,6 @@ df_1990_2001.to_csv("/nfs/a319/gy17m2a/Outputs/TimeSeries/CEH-GEAR/Armley/TimeSe
 
 ### Save the coordinates for which the data was extracted
 coordinates_str = "latitude: " + str(lat) + ", longitude: " + str(lon)
-f =open("/nfs/a319/gy17m2a/Outputs/TimeSeries/CEH-GEAR/Armley/location_coordinates.txt", "w")
+f =open("/nfs/a319/gy17m2a/Outputs/TimeSeries/CEH-GEAR/{}/location_coordinates.txt".format(location), "w")
 f.write(coordinates_str)
 f.close()
