acdc9d37262ca45a9c7fa8c17d97d275ccbbe19c
Molly <m.asher@hotmail.com>
2021-03-02 14:36:35 +0000

    catchment analysis stuff

--- CatchmentAnalysis/CatchmentData.py
+++ CatchmentAnalysis/CatchmentData.py
@@ -71,75 +71,25 @@ plt.scatter(transposed['ALTBAR'], transposed['SAAR'])
 plt.xlabel('Mean Catchment Altitude (m above sea level)')
 plt.ylabel('Standard Average Areal Rainfall (mm)')
 
+##################################################################
+##################################################################
+
+# Define rootfp and set as working directory
+root_fp = "C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/FloodModelling/IndividualCatchments/"
+os.chdir(root_fp)
+
+#########
+catchment_names  = glob.glob("*")
+for catchment_name in catchment_names:
+    print(catchment_name)
+    filename = glob.glob(root_fp + "{}/DesignRainfall/*.csv".format(catchment_name))[0]
+    design_rainfall = pd.read_csv(filename, skiprows =9)
 
 
-# Firgreen Beck
-urbext = [0.0165]
-saar6190 = [690]
-propwet = [0.32]
-area = [34.54] #km2
+    plt.bar(design_rainfall['Duration hours'], height =design_rainfall['2 year rainfall (mm)'] )
 
-# Guiseley Beck
-urbext = [0.2628]
-saar6190 = [829]
-propwet = [0.32]
-area = [13.935] #km2
-
-# Oil Mill Beck
-urbext = [0.2141]
-saar6190 = [801]
-propwet = [0.32]
-area = [16.27] #km2
-
-# Lin Dyke
-urbext = [0.1321]
-saar6190 = [652]
-propwet = [0.32]
-area = [20.36] #km2
-
-# Gill Beck- Aire
-urbext = [0.0199]
-saar6190 = [894]
-propwet = [0.32]
-area = [13.94] #km2
-
-# Lin Dyke rainfall data
-urbext = [0.1321]
-saar6190 = [652]
-propwet = [0.32]
-area = [20.36] #km2
-
-# Holbeck rainfall data
-urbext = [0.2822]
-saar6190 = [762]
-propwet = [0.32]
-area = [62.56] #km2
-
-# Meanwood Beck data
-urbext = [0.3605]
-saar6190 = [753]
-propwet = [0.32]
-area = [42.57] #km2
-
-# Mill Beck data
-urbext = [0.0458]
-saar6190 = [657]
-propwet = [0.32]
-area = [7.29] #km2
-
-# Wyke Beck data
-urbext = [0.3427]
-saar6190 = [706]
-propwet = [0.32]
-area = [27.29] #km2
-
-# Fairbun Ings rainfall data
-urbext2000 = [0.0102]
-saar6190 = [628]
-propwet = [0.32]
-area = [13.35] #km2
-durations = [1,3,5,7, 9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]
-total_rainfall = [4.45, 9.12, 12.05, 14.14, 15.59,16.81,17.86, 18.8,19.64,20.41, 21.13,21.80,22.42,22.97,23.49, 23.99, 24.47,
-                  24.92, 25.36, 25.77]
-peak_rainfall = [1.97, 1.56, 1.26, 1.06, 0.91, 0.81,0.73,0.66, 0.61, 0.57, 0.53, 0.50, 0.47, 0.45,0.43, 0.41, 0.39, 0.38,
-                 0.36, 0.35]
+    
+# Rename columns
+df.rename(columns={"VERSION": "Catchment Descriptor"}, inplace = True)
+# Delete extra column
+del df[' "FEH CD-ROM"']  
--- CatchmentAnalysis/CatchmentLocations.py
+++ CatchmentAnalysis/CatchmentLocations.py
@@ -63,8 +63,6 @@ transposed = transposed[1:]
 transposed = transposed.reset_index()
 concat_shps = concat_shps.reset_index()
 transposed['name'] = concat_shps['name']
-# Convert to numeric]
-transposed[:,1:38] = transposed[:,1:38].apply(pd.to_numeric)
 
 cols = transposed.columns[1:38]
 transposed[cols] = transposed[cols].apply(pd.to_numeric, errors='coerce', axis=1)
@@ -75,14 +73,48 @@ concat_shps = concat_shps.merge(transposed, on='name')
 # Plot
 concat_shps.plot()
 
-fig, ax = plt.subplots(1, 1)
-#concat_shps.plot(column='ALTBAR', ax=ax, legend=True)
-concat_shps.plot(column='ALTBAR',   cmap='OrRd',
-             legend=True)
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+ax.axis('off')
+concat_shps.plot(ax=ax, column='ALTBAR',   cmap='OrRd', edgecolor = 'black',
+             legend=True,legend_kwds={'label': "Metres above sea level",
+                                      'orientation': "vertical"})
+# Save and show plot
+plt.savefig("C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/Scripts/UKCP18/CatchmentAnalysis/Figs/Catchments_matching_FEH_ALTBAR_spatialplot.PNG",
+            bbox_inches='tight')
+plt.show()
 
 
-fig, ax = plt.subplots(1, 1)
-#concat_shps.plot(column='ALTBAR', ax=ax, legend=True)
-concat_shps.plot(column='URBEXT2000')   cmap='OrRd',
-             legend=True)
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+ax.axis('off')
+concat_shps.plot(ax=ax, column='URBEXT2000',   cmap='OrRd', edgecolor = 'black',
+             legend=True,legend_kwds={'label': "Urban Extent",
+                                      'orientation': "vertical"})
+# Save and show plot
+plt.savefig("C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/Scripts/UKCP18/CatchmentAnalysis/Figs/Catchments_matching_FEH_URBEXT_spatialplot.PNG",
+            bbox_inches='tight')
+plt.show()
 
+
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+ax.axis('off')
+concat_shps.plot(ax=ax, column='BFIHOST',   cmap='OrRd', edgecolor = 'black',
+             legend=True,legend_kwds={'label': "BFIHOST",
+                                      'orientation': "vertical"})
+# Save and show plot
+plt.savefig("C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/Scripts/UKCP18/CatchmentAnalysis/Figs/Catchments_matching_FEH_BFIHOST_spatialplot.PNG",
+            bbox_inches='tight')
+plt.show()
+
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+ax.axis('off')
+concat_shps.plot(ax=ax, column='SAAR',   cmap='OrRd', edgecolor = 'black',
+             legend=True,legend_kwds={'label': "SAAR (mm)",
+                                      'orientation': "vertical"})
+# Save and show plot
+plt.savefig("C:/Users/gy17m2a/OneDrive - University of Leeds/PhD/DataAnalysis/Scripts/UKCP18/CatchmentAnalysis/Figs/Catchments_matching_FEH_SAAR_spatialplot.PNG",
+            bbox_inches='tight')
+plt.show()
